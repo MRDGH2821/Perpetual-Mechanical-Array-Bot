9c2823a625794c796e2dcd0f9f5b7e9b4b82d520
MRDGH2821 <mihirrabade@gmail.com>
2021-12-06 11:52:40 +0530

    Added event & command execution code

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const fs = require("fs");
+const { Client, Collection, Intents } = require("discord.js");
+const { token } = require("./config.json");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter(file => file.endsWith(".js"));
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
-	const command = client.commands.get(interaction.commandName);
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isCommand()) return;
 
-	if (!command) return;
+  const command = client.commands.get(interaction.commandName);
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    return interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true
+    });
+  }
 });
 
 client.login(token);
