717b3df0d0a4341675f38899aa1d9811eda0168f
MRDGH2821 <mihirrabade@gmail.com>
2021-12-06 11:50:20 +0530

    Files beautified

--- commands/bonk.js
+++ commands/bonk.js
@@ -1,12 +1,17 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('bonk')
-		.setDescription('Select a member and bonk them.')
-		.addUserOption(option => option.setName('target').setDescription('The member to bonk')),
-	async execute(interaction) {
-		const user = interaction.options.getUser('target');
-		return interaction.reply({ content: `${user.username} has been bonked!` });
-	},
+  data: new SlashCommandBuilder()
+    .setName("bonk")
+    .setDescription("Select a member and bonk them.")
+    .addUserOption(option =>
+      option
+        .setName("target")
+        .setDescription("The member to bonk")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const user = interaction.options.getUser("target");
+    return interaction.reply({ content: `${user.username} has been bonked!` });
+  }
 };
