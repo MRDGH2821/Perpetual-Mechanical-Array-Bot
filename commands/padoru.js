76f3c212513723c1cd89bff885e568968dde4619
MRDGH2821 <mihirrabade@gmail.com>
2021-12-07 18:57:34 +0530

    Updated padoru command

--- commands/padoru.js
+++ commands/padoru.js
@@ -7,8 +7,17 @@ export default new Command({
   },
 
   async run(interaction) {
+    const channel = interaction.channel;
     await interaction.reply({
-      content: `<@&813613841488936971> Hashire sori yo\nKazeno yuu ni\nTsukkimihara wo\nPADORU PADORU\n<:LuminePadoru:912033737280192562><:LuminePadoru:912033737280192562><:LuminePadoru:912033737280192562><:LuminePadoru:912033737280192562>`
+      content: "Sending",
+      ephemeral: true
     });
+    await channel.send("<@&813613841488936971> Hashire sori yo");
+    await channel.send("<@&813613841488936971> Kazeno yuu ni");
+    await channel.send("<@&813613841488936971> Tsukkimihara wo");
+    await channel.send("<@&813613841488936971> PADORU PADORU");
+    await channel.send(
+      "<:LuminePadoru:912033737280192562><:LuminePadoru:912033737280192562><:LuminePadoru:912033737280192562><:LuminePadoru:912033737280192562>"
+    );
   }
 });
