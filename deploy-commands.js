5c56dc276383987c14dc719187a582c2e83ee06a
MRDGH2821 <mihirrabade@gmail.com>
2021-12-07 13:56:20 +0530

    Added events & commmands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,9 +1,11 @@
 import { Module } from "@ruinguard/core";
 import essentials from "@ruinguard/essentials";
+import commands from "./CommandIndex.js";
+import events from "./EventsIndex.js";
 import { config as ENV } from "dotenv";
 ENV();
 
-await Module.registerGuildCommands([essentials], {
+await Module.registerGuildCommands([essentials , commands, events], {
   app: process.env.CLIENTID,
   guild: process.env.GUILDID,
   token: process.env.TOKEN
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { Bot } from "@ruinguard/core";
 import essentials from "@ruinguard/essentials";
-
+import events from "./EventsIndex.js";
+import commands from "./CommandIndex.js";
 import { config as ENV } from "dotenv";
 ENV();
 
 const bot = new Bot({
-  modules: [essentials]
+  modules: [essentials, events, commands]
 });
 
 await bot.login(process.env.TOKEN);
