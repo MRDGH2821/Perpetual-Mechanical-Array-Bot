7e8441de2ac62a94482074021805d32859157bd4
MRDGH2821 <mihirrabade@gmail.com>
2022-01-18 19:10:43 +0530

    refactor(command): use slash command builder

--- commands/amc.js
+++ commands/amc.js
@@ -1,50 +1,35 @@
-import { Command } from "@ruinguard/core";
+import { Command } from '@ruinguard/core';
+import { SlashCommandBuilder } from '@discordjs/builders';
 
+const cmd = new SlashCommandBuilder()
+  .setName('amc')
+  .setDescription('Anemo Main Character')
+  .addSubcommand(subcommand => subcommand
+    .setName('gust_surge')
+    .setDescription('AMC Burst')
+    .addStringOption(option => option
+      .setName('techs')
+      .setDescription('Technologies which power burst')
+      .setRequired(true)
+      .addChoice('Cyronado: Guoba', 'cyronadoguoba')
+      .addChoice('Cyronado: Baron Bunny', 'cyronadobaron')));
 export default new Command({
-  data: {
-    name: "amc",
-    description: "Anemo Main Character",
-    options: [
-      {
-        type: 1,
-        name: "gust_surge",
-        description: "AMC Burst",
-        options: [
-          {
-            type: 3,
-            name: "techs",
-            description: "Technologies which power burst",
-            required: true,
-            choices: [
-              {
-                value: "cyronadoguoba",
-                name: "Cyronado: Guoba"
-              },
-              {
-                value: "cyronadobaron",
-                name: "Cyronado: Baron Bunny"
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  },
+  data: cmd,
   async run(interaction) {
-    await interaction.deferReply("gmc");
-    const option = interaction.options.getString("techs");
+    await interaction.deferReply('gmc');
+    const option = interaction.options.getString('techs');
     let gif;
     let name;
     switch (option) {
-      case "cyronadoguoba":
-        gif = "https://i.imgur.com/v2OWCkz.mp4";
-        name = "Cyronado: Guoba";
-        break;
-      case "cyronadobaron":
-        gif = "https://i.imgur.com/sjEmHjY.mp4";
-        name = "Cyronado: Baron Bunny";
-        break;
+    case 'cyronadoguoba':
+      gif = 'https://i.imgur.com/v2OWCkz.mp4';
+      name = 'Cyronado: Guoba';
+      break;
+    case 'cyronadobaron':
+      gif = 'https://i.imgur.com/sjEmHjY.mp4';
+      name = 'Cyronado: Baron Bunny';
+      break;
     }
     await interaction.editReply({ content: `**${name}**\n\n${gif}` });
-  }
+  },
 });
