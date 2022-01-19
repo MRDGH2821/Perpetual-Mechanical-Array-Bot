0f21af1ff8a57e2d514499c963fbb51f6b09298b
MRDGH2821 <mihirrabade@gmail.com>
2022-01-19 16:33:52 +0530

    refactor: refactor inputs

--- commands/joke.js
+++ commands/joke.js
@@ -10,11 +10,15 @@ const blacklist
 const cmd = new SlashCommandBuilder()
   .setName('joke')
   .setDescription('Get a random joke!')
-  .addBooleanOption(option => option.setName('programmming').setDescription('Get programmming joke'))
-  .addBooleanOption(option => option.setName('misc').setDescription('Get Misc joke'))
-  .addBooleanOption(option => option.setName('pun').setDescription('Get pun joke'))
-  .addBooleanOption(option => option.setName('spooky').setDescription('Get spooky joke'))
-  .addBooleanOption(option => option.setName('christmas').setDescription('Get christmas joke'));
+  .addStringOption(option => option
+    .setName('type')
+    .setDescription('Select category of joke')
+    .addChoice('Programming', 'Programming')
+    .addChoice('Misc', 'Misc')
+    .addChoice('Pun', 'Pun')
+    .addChoice('Spooky', 'Spooky')
+    .addChoice('Christmas', 'Christmas')
+    .addChoice('Any', 'Any'));
 
 const submitJoke = new MessageActionRow().addComponents(new MessageButton()
   .setLabel('Submit a joke!')
@@ -24,52 +28,26 @@ const submitJoke = new MessageActionRow().addComponents(new MessageButton()
 export default new Command({
   data: cmd,
   async run(interaction) {
-    const programming = await interaction.options.getBoolean('programming');
-    const misc = await interaction.options.getBoolean('misc');
-    const pun = await interaction.options.getBoolean('pun');
-    const spooky = await interaction.options.getBoolean('spooky');
-    const christmas = await interaction.options.getBoolean('christmas');
+    const type = (await interaction.options.getString('type')) || 'Any';
 
-    let finalQuery = '';
-
-    if (programming && misc && pun && spooky && christmas) {
-      finalQuery = 'Any';
-    } else if (!programming || !misc || !pun || !spooky || !christmas) {
-      finalQuery = 'Any';
-    } else {
-      const query = [];
-      if (programming) {
-        query.concat(['Programming']);
-      }
-      if (misc) {
-        query.concat(['Misc']);
-      }
-      if (pun) {
-        query.concat(['Pun']);
-      }
-      if (spooky) {
-        query.concat(['Spooky']);
-      }
-      if (christmas) {
-        query.concat(['Christmas']);
-      }
-      const temp = JSON.stringify(query);
-      console.log('temp: ', temp);
-
-      finalQuery = temp.substring(1, temp.length - 1);
-    }
-    console.log('Final Query: ', finalQuery);
-
-    const apiURL = `${baseAPI + finalQuery + blacklist}&format=txt&safe-mode`;
+    const apiURL = `${baseAPI + type + blacklist}&safe-mode`;
     console.log('API URL: ', apiURL);
 
-    const joke = await axios.get(apiURL);
-    console.log('Joke: ', joke.data);
+    const { data } = await axios.get(apiURL);
+    console.log('Joke: ', data);
+
+    let joke = '';
+    if (data.type === 'single') {
+      joke = `${data.joke}`;
+    } else {
+      joke = `${data.setup}\n\n${data.delivery}`;
+    }
 
     const jokeEmbed = new MessageEmbed()
       .setColor('#524437')
       .setTitle('**Joke!**')
-      .setDescription(`${joke.data}`);
+      .setDescription(`${joke}`)
+      .addField('**Type & ID**', `${data.category}, ${data.id}`);
 
     await interaction.reply({
       embeds: [jokeEmbed],
