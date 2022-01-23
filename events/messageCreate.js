ab3951876fe7c77561798ee64aa86418cb14d9e1
MRDGH2821 <mihirrabade@gmail.com>
2022-01-23 23:49:32 +0530

    feat: add internal cooldown for auto bonk

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,22 @@
-import { Event } from '@ruinguard/core';
+import { Event, CooldownManager } from '@ruinguard/core';
 import Bonk from '../auto-reply/bonk.js';
+const icd = new CooldownManager();
+icd.add('bonkCD', 0);
 
 export default new Event({
   event: 'messageCreate',
   async run(message) {
     const bonk = new Bonk(message);
-    console.log(message.content);
-    await bonk.hornyBonk();
+    // console.log(message.content);
+    if (bonk.isHorny()) {
+      const timeLeft = await icd.check('bonkCD');
+      console.log('ICD: ', timeLeft);
+      if (timeLeft < 1 || timeLeft === false) {
+        await bonk.hornyBonk();
+        await icd.add('bonkCD', 60000);
+      } else {
+        console.log('Reached else part, Bonk CD: ', await icd.check('bonkCD'));
+      }
+    }
   },
 });
