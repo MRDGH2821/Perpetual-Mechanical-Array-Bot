6405c4196aa69fce2842bded9fef5337c9ef2185
MRDGH2821 <mihirrabade@gmail.com>
2022-02-05 18:14:36 +0530

    chore: add deployment app

--- pma-bot.pm2.cjs
+++ pma-bot.pm2.cjs
@@ -1,9 +1,18 @@
+/* eslint-disable camelcase */
 module.exports = {
   apps: [
     {
+      env_dev: { NODE_ENV: 'dev' },
+      env_prod: { NODE_ENV: 'prod' },
       name: 'PMA-Bot',
       script: './index.js',
       watch: '.',
+    },
+    {
+      autorestart: false,
+      name: 'Deploy PMA-Bot',
+      script: './deploy-check.cjs',
+      watch: false,
     },
   ],
 };
