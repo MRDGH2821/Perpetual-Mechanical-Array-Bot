cfa1028053c6968322160541d2fbb0caa89669b6
MRDGH2821 <mihirrabade@gmail.com>
2022-02-24 21:12:50 +0530

    feat: add subcommand - guide

--- commands/amc.js
+++ commands/amc.js
@@ -1,4 +1,6 @@
 import { Command } from '@ruinguard/core';
+// eslint-disable-next-line no-unused-vars
+import { CommandInteraction } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 
 const cmd = new SlashCommandBuilder()
@@ -12,24 +14,48 @@ const cmd = new SlashCommandBuilder()
       .setDescription('Technologies which power burst')
       .setRequired(true)
       .addChoice('Cyronado: Guoba', 'cyronadoguoba')
-      .addChoice('Cyronado: Baron Bunny', 'cyronadobaron')));
+      .addChoice('Cyronado: Baron Bunny', 'cyronadobaron')))
+  .addSubcommand((subcommand) => subcommand.setName('guide').setDescription('Guide on AMC'));
+
 export default new Command({
   data: cmd,
+
+  /**
+   * show amc techs & guide
+   * @async
+   * @function run
+   * @param {CommandInteraction} interaction - interaction object
+   */
   async run(interaction) {
-    await interaction.deferReply('gmc');
-    const option = interaction.options.getString('techs');
-    let gif,
-      name;
-    switch (option) {
-    case 'cyronadoguoba':
-      gif = 'https://i.imgur.com/v2OWCkz.mp4';
-      name = 'Cyronado: Guoba';
-      break;
-    case 'cyronadobaron':
-      gif = 'https://i.imgur.com/sjEmHjY.mp4';
-      name = 'Cyronado: Baron Bunny';
+    await interaction.deferReply();
+    switch (await interaction.options.getSubcommand()) {
+    case 'gust_surge': {
+      const option = interaction.options.getString('techs');
+      let gif = '',
+        name = '';
+      switch (option) {
+      case 'cyronadoguoba': {
+        gif = 'https://i.imgur.com/v2OWCkz.mp4';
+        name = 'Cyronado: Guoba';
+        break;
+      }
+      case 'cyronadobaron': {
+        gif = 'https://i.imgur.com/sjEmHjY.gif';
+        name = 'Cyronado: Baron Bunny';
+        break;
+      }
+          // no default
+      }
+      await interaction.editReply({
+        content: `**${name}**\n\n${gif}`
+      });
       break;
     }
-    await interaction.editReply({ content: `**${name}**\n\n${gif}` });
+    case 'guide': {
+      await interaction.editReply('https://keqingmains.com/anemo-traveler/');
+      break;
+    }
+      // no default
+    }
   }
 });
