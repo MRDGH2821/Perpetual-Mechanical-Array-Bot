ad12cc0815b61273876d92ce64bb7e430cc17ccf
MRDGH2821 <mihirrabade@gmail.com>
2022-02-25 00:13:21 +0530

    chore: rename scripts

--- pma-bot.config.cjs
+++ pma-bot.config.cjs
@@ -5,13 +5,13 @@ module.exports = {
       env_dev: { NODE_ENV: 'dev' },
       env_prod: { NODE_ENV: 'prod' },
       name: 'PMA-Bot',
-      script: './index.js',
+      script: 'index.js',
       watch: true
     },
     {
       autorestart: false,
       name: 'Deploy PMA-Bot',
-      script: './deploy-check.cjs',
+      script: 'deploy-check.cjs',
       watch: false
     }
   ]
