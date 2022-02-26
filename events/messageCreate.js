98adb38e6d3e322c816e58d449e2cfc3afc79c0a
MRDGH2821 <mihirrabade@gmail.com>
2022-02-26 23:50:58 +0530

    feat: split auto-bonk into trigger

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,25 +1,20 @@
-import { CooldownManager, Event } from '@ruinguard/core';
-import Bonk from '../auto-reply/bonk.js';
+import { Event } from '@ruinguard/core';
+
+// eslint-disable-next-line no-unused-vars
+import { Message } from 'discord.js';
 import react from '../auto-reply/react.js';
-const icd = new CooldownManager();
-icd.add('bonkCD', 0);
 
 export default new Event({
   event: 'messageCreate',
+
+  /**
+   * message create event
+   * @async
+   * @function run
+   * @param {Message} message - message object
+   */
   async run(message) {
-    react(message);
-    const bonk = new Bonk(message);
     // console.log(message.content);
-    if (bonk.isHorny()) {
-      const timeLeft = await icd.check('bonkCD');
-      console.log('ICD: ', timeLeft);
-      if (timeLeft < 1 || timeLeft === false) {
-        await bonk.hornyBonk();
-        await icd.add('bonkCD', 300000);
-      }
-      else {
-        console.log('Reached else part, Bonk CD: ', await icd.check('bonkCD'));
-      }
-    }
+    await react(message);
   }
 });
---
+++ triggers/auto-bonk.js
@@ -0,0 +1,44 @@
+import { CooldownManager, Event, MessageEmbed } from '@ruinguard/core';
+import Bonk from '../lib/bonk-utilities.js';
+import { EmbedColor } from '../lib/constants.js';
+// eslint-disable-next-line no-unused-vars
+import { Message } from 'discord.js';
+const fiveMins = 300000,
+  icd = new CooldownManager();
+
+// eslint-disable-next-line no-magic-numbers
+icd.add('bonkCD', 0);
+
+export default new Event({
+  event: 'messageCreate',
+
+  /**
+   * message create event
+   * @async
+   * @function run
+   * @param {Message} message - message object
+   */
+  async run(message) {
+    const bonk = new Bonk(message.content);
+
+    if (bonk.isHorny(message.content)) {
+      const embedReply = new MessageEmbed()
+          .setColor(EmbedColor)
+          .setTitle('**Bonked!**')
+          .setDescription(bonk.bonkHornyReason())
+          .setImage(bonk.hornyBonkGif()),
+        timeLeft = await icd.check('bonkCD');
+      console.log('ICD: ', timeLeft);
+      // eslint-disable-next-line no-magic-numbers
+      if (timeLeft < 1 || timeLeft === false) {
+        await message.reply({
+          embeds: [embedReply]
+        });
+        await icd.add('bonkCD', fiveMins);
+      }
+      else {
+        console.log('Reached else part, Bonk CD: ', await icd.check('bonkCD'));
+      }
+    }
+  }
+});
