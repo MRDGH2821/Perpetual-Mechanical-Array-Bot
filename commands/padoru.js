00c46f21bcb15c9a16b20f52b83ec36e1c217103
MRDGH2821 <mihirrabade@gmail.com>
2022-02-26 19:18:40 +0530

    feat: restrict command to botspam & command center
    Also use formatters, send sticker & add JSDocs

--- commands/padoru.js
+++ commands/padoru.js
@@ -1,21 +1,45 @@
+import {
+  SlashCommandBuilder,
+  channelMention,
+  formatEmoji,
+  roleMention
+} from '@discordjs/builders';
+import { botSpamID, commandCenterID } from '../lib/channelIDs.js';
 import { Command } from '@ruinguard/core';
+// eslint-disable-next-line no-unused-vars
+import { CommandInteraction } from 'discord.js';
+import { LuminePadoru } from '../lib/emoteIDs.js';
+import { archonsID } from '../lib/roleIDs.js';
 
 export default new Command({
-  data: {
-    name: 'padoru',
-    description: 'Will sing Padoru (as text)'
-  },
-  flags: [1 << 3],
+  data: new SlashCommandBuilder()
+    .setName('padoru')
+    .setDescription('Will sing Padoru (as text)'),
+
+  /**
+   * will sing padoru as text
+   * @async
+   * @function run
+   * @param {CommandInteraction} interaction - interaction object
+   */
   async run(interaction) {
     const { channel } = interaction;
-    await interaction.reply({
-      content: `Merry Christmas ${interaction.user.tag}!`
-    });
-    await channel.send('<@&813613841488936971> Hashire sori yo');
-    await channel.send('<@&813613841488936971> Kazeno yuu ni');
-    await channel.send('<@&813613841488936971> Tsukkimihara wo');
-    await channel.send('<@&813613841488936971> PADORU PADORU');
-    await channel.send('<:LuminePadoru:912033737280192562><:LuminePadoru:912033737280192562><:LuminePadoru:912033737280192562><:LuminePadoru:912033737280192562>');
-    //  await channel.send({ stickers: [917799217320316928] });
+    if (channel.id === botSpamID || channel.id === commandCenterID) {
+      await interaction.reply({
+        content: `Merry Christmas ${interaction.user.tag}!`
+      });
+      await channel.send(`${roleMention(archonsID)} Hashire sori yo`);
+      await channel.send(`${roleMention(archonsID)} Kazeno yuu ni`);
+      await channel.send(`${roleMention(archonsID)} Tsukkimihara wo`);
+      await channel.send(`${roleMention(archonsID)} PADORU PADORU`);
+      await channel.send(`${formatEmoji(LuminePadoru)}${formatEmoji(LuminePadoru)}${formatEmoji(LuminePadoru)}${formatEmoji(LuminePadoru)}`);
+      await channel.send({ stickers: ['912034014112649276'] });
+    }
+    else {
+      await interaction.reply({
+        content: `Please use ${channelMention(botSpamID)}`,
+        ephemeral: true
+      });
+    }
   }
 });
