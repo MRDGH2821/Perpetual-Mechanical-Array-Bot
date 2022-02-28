ac5c785c64ef21b2c6da4aa531b8e8b396cfa569
MRDGH2821 <mihirrabade@gmail.com>
2022-03-01 00:28:00 +0530

    wip: investigate crashes on button interaction

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import { Interaction, MessageEmbed } from 'discord.js';
 import { EmbedColor } from '../lib/constants.js';
@@ -14,42 +15,51 @@ export default new Event({
    * @param {Interaction} interaction - interaction object
    */
   async run(interaction) {
-    if (!interaction.isAutocomplete()) {
-      console.log('------');
+    // eslint-disable-next-line padded-blocks
+    try {
 
-      console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.\nUser ID: ${interaction.user.id}, Command: ${interaction.commandName}\n`);
+      /*
+      if (interaction.isApplicationCommand()) {
+        console.log('------');
 
-      console.log('---\nCommand Logs:');
+        console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.\nUser ID: ${interaction.user.id}, Command: ${interaction.commandName}\n`);
 
-      const avatarUrl = interaction.user.displayAvatarURL({
-          dynamic: true
-        }),
-        logEmbed = new MessageEmbed()
-          .setTitle('**Interaction log**')
-          .setAuthor({
-            iconURL: avatarUrl,
-            name: interaction.user.tag
-          })
-          .setColor(EmbedColor)
-          .setThumbnail(avatarUrl)
-          .setDescription(`${interaction.user} in ${interaction.channel} triggered an interaction.`)
-          .addFields([
-            {
-              name: '**Command Name**',
-              value: `${interaction.commandName}`
-            },
-            {
-              name: '**User ID**',
-              value: `${interaction.user.id}`
-            }
-          ]),
-        logchannel = await interaction.guild.channels.fetch(archivesID);
+        console.log('---\nCommand Logs:');
 
-      logchannel.send({
-        embeds: [logEmbed]
-      });
+        const avatarUrl = interaction.user.displayAvatarURL({
+            dynamic: true
+          }),
+          logEmbed = new MessageEmbed()
+            .setTitle('**Interaction log**')
+            .setAuthor({
+              iconURL: avatarUrl,
+              name: interaction.user.tag
+            })
+            .setColor(EmbedColor)
+            .setThumbnail(avatarUrl)
+            .setDescription(`${interaction.user} in ${interaction.channel} triggered an interaction.`)
+            .addFields([
+              {
+                name: '**Command Name**',
+                value: `${interaction.commandName}`
+              },
+              {
+                name: '**User ID**',
+                value: `${interaction.user.id}`
+              }
+            ]),
+          logchannel = await interaction.guild.channels.fetch(archivesID);
+
+        logchannel.send({
+          embeds: [logEmbed]
+        });
+      }
+      */
+      // eslint-disable-next-line no-underscore-dangle
+      await interaction.client._onInteractionCreate(interaction);
     }
-    // eslint-disable-next-line no-underscore-dangle
-    interaction.client._onInteractionCreate(interaction);
+    catch (error) {
+      console.error(error);
+    }
   }
 });
