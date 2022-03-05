79a46f164eb36bf4fe848fa7895f7a23477fcb4d
MRDGH2821 <mihirrabade@gmail.com>
2022-03-05 13:46:18 +0530

    chore: update JSDocs
    Returns array of objects

--- filesExporter.js
+++ filesExporter.js
@@ -3,18 +3,18 @@ import { pathToFileURL } from 'url';
 import { resolve } from 'path';
 
 /**
- * returns array of file paths in given folder path
+ * returns array of file imports in given folder path
  * @function arrayOfFilesGenerator
  * @param {string} folderPath - folder path
- * @returns {Array[string]} - array of file paths in given folder
+ * @returns {Array[object]} - array of file imports in given folder
  */
 export function arrayOfFilesGenerator(folderPath) {
   const arrayOfFiles = [],
     folder = resolve(folderPath);
   return fs.readdir(folder, (err, files) => {
     files.forEach(async(file) => {
-      // console.log(file);
       const jsfile = await import(pathToFileURL(`${folder}\\${file}`));
+      // console.log(jsfile);
       arrayOfFiles.push(jsfile);
     });
     // console.log(arrayOfFiles);
