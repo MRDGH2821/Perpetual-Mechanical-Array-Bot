23d5d508c4d52ecede7f7acf149dec675e22ea63
MRDGH2821 <mihirrabade@gmail.com>
2022-03-05 14:49:22 +0530

    chore: uninstall ruinguard essentials module

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,6 @@
 import { Module } from '@ruinguard/core';
 import commands from './PMAIndex.js';
 import { configuration } from './lib/ConfigManager.js';
-import essentials from '@ruinguard/essentials';
 
 await Module.registerGuildCommands([commands], {
   app: configuration.clientId,
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { Bot } from '@ruinguard/core';
 import commands from './PMAIndex.js';
 import { configuration } from './lib/ConfigManager.js';
-import essentials from '@ruinguard/essentials';
+// import triggers from './TriggerIndex.js';
 
 const bot = new Bot({ modules: [commands] });
 
