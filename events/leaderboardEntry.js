d20f1b9f884d8d7e5e5a718a8fc9103a799c7be7
MRDGH2821 <mihirrabade@gmail.com>
2022-03-05 23:22:53 +0530

    feat: add leaderboard command
    It comes with link & bot validation & saves to database

---
+++ events/leaderboardEntry.js
@@ -0,0 +1,39 @@
+import { Event } from '@ruinguard/core';
+// eslint-disable-next-line no-unused-vars
+import { User } from 'discord.js';
+import { db } from '../lib/firebase.cjs';
+
+export default new Event({
+  event: 'leaderboardEntry',
+
+  /**
+   * submits score for leaderboard
+   * @function run
+   * @param {User} target - user whose score is registered
+   * @param {object} submission - properties of submission
+   * @param {string} submission.elementCategory - elemental category
+   * @param {string} submission.proof - submission proof link
+   * @param {number} submission.score - damage score
+   * @param {string} submission.typeCategory - solo or open category
+   */
+  async run(target, { elementCategory, proof, score, typeCategory }) {
+    console.log('Entry received, saving to database');
+    const collectionName = `${elementCategory}-${typeCategory}`,
+      submission = {
+        elementCategory,
+        proof,
+        score,
+        typeCategory,
+        userID: target.id
+      };
+    await db
+      .collection(collectionName)
+      .doc(target.id)
+      .set(submission)
+      .then(() => console.log('Leaderboard Entry submitted!'))
+      .catch((error) => {
+        console.log('An error occurred while submitting leaderboard entry');
+        console.error(error);
+      });
+  }
+});
