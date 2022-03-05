ecc9c58b5d571405523f078184ca3739af326c2a
MRDGH2821 <mihirrabade@gmail.com>
2022-03-05 19:10:15 +0530

    feat: add triggers

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import { Bot } from '@ruinguard/core';
 import commands from './PMAIndex.js';
 import { configuration } from './lib/ConfigManager.js';
-// import triggers from './TriggerIndex.js';
+import triggers from './TriggerIndex.js';
 
-const bot = new Bot({ modules: [commands] });
+const bot = new Bot({ modules: [
+  commands,
+  triggers
+] });
 
 await bot.login(configuration.token);
