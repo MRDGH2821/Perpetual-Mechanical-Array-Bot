dc50c3dbda24d01d89e70ed954452f13fe6d1ffb
MRDGH2821 <mihirrabade@gmail.com>
2022-03-06 13:51:25 +0530

    chore: update help command
    Made field titles bold
    Add dmg_leaderboard help
    Update description of calculate expression

--- commands/help.js
+++ commands/help.js
@@ -23,45 +23,49 @@ export default new Command({
       .setTitle('Help')
       .setDescription(`Need Help?\nBot version = ${json.version}`)
       .addFields(
-        { name: '/amc', value: 'Shows various AMC techs' },
+        { name: '**/amc**', value: 'Shows various AMC techs' },
         {
-          name: '/bonk',
+          name: '**/bonk**',
           value: `Bonk a user ${KEKW}\n(You can also specify a reason ${worrySmart})`
         },
         {
-          name: '/calculator normal',
+          name: '**/calculator normal**',
           value:
-            'Your regular calculator. Usage is similar to [Math Notepad](https://mathnotepad.com/docs/overview.html). Some features might not be available like variables. This command uses [Math.js](https://api.mathjs.org/) API to solve expressions.'
+            'Your regular calculator. Usage is similar to [Math Notepad](https://mathnotepad.com/docs/overview.html). Some features might not be available like variables. This command uses [Math.js](https://mathjs.org/docs/expressions/parsing.html) Evaluate function to solve expressions.'
         },
         {
-          name: '/calculator dmg_compare',
+          name: '**/calculator dmg_compare**',
           value:
             'This command allows you to compare two sets based only on three factors, namely Attack, Crit Rate and Crit Damage. \nThis serves as a simple way to compare sets, but is not the only way to evaluate which set is better since other variables (such as external bonuses or buffs) can exist. \nIdeally, you would want a damage value above 4000 for main dps units, and well-invested units tend to reach 5000+ damage values.\n\n**Note:** Damage values might vary in-game.'
         },
         {
-          name: '/calculator damage',
+          name: '**/calculator damage**',
           value:
             'This command allows you to calculate an estimated "damage" that you can achieve from three stats: Attack, Crit Rate and Crit Damage. \nIdeally, you would want a damage value above 4000 for main dps units, and well-invested units tend to reach 5000+ above damage values.\n\n**Note:** Damage values might vary in-game.'
         },
         {
-          name: '/give-role',
+          name: '**/dmg_leaderboard**',
+          value: 'Submit your entry for damage leaderboard!'
+        },
+        {
+          name: '**/give-role & /give-one-role**',
           value: 'A mod only command which gives roles to users'
         },
         {
-          name: '/gmc',
+          name: '**/gmc**',
           value: 'Shows various GMC techs'
         },
         {
-          name: '/help',
+          name: '**/help**',
           value: 'The help command'
         },
         {
-          name: '/joke',
+          name: '**/joke**',
           value:
             'Tells you a joke from given categories: Programming, Misc, Pun, Spooky & Christmas. By default any random category will be picked.'
         },
         {
-          name: '/padoru',
+          name: '**/padoru**',
           value: 'Sings padoru as text'
         }
       );
