c921e1074d06f081383f4f262ffd85ee82d4bc1e
MRDGH2821 <mihirrabade@gmail.com>
2022-03-21 19:27:06 +0530

    wip: add hall of fame updater

---
+++ events/hofUpdate.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import { Client } from "discord.js";
+import { Event } from "@ruinguard/core";
+import { hallOfFameGenerate } from "../lib/HallOfFameManager.js";
+export default new Event({
+  event: "hofUpdate",
+
+  /**
+   * updates Hall of fame embeds with new data
+   * @async
+   * @function run
+   * @param {Client} client - client object
+   */
+  async run(client) {
+    console.log("Starting leaderboard update.");
+
+    const anemoCrownBoard = await hallOfFameGenerate(client, "anemo-crown");
+  }
+});
