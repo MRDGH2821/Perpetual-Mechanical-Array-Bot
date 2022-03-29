bdb1cfe0d29cf70d2363a5d90d299a53f801cd45
MRDGH2821 <mihirrabade@gmail.com>
2022-03-29 23:38:50 +0530

    feat: show sub command name in logs

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,6 @@
 import { AMCTechs, GMCTechs } from "../lib/TravelerTechnologies.js";
 // eslint-disable-next-line no-unused-vars
-import { Interaction, MessageEmbed } from "discord.js";
+import { CommandInteraction, MessageEmbed } from "discord.js";
 import { EmbedColor } from "../lib/constants.js";
 import { Event } from "@ruinguard/core";
 import { archivesID } from "../lib/channelIDs.js";
@@ -12,14 +12,20 @@ export default new Event({
    * interaction event
    * @async
    * @function run
-   * @param {Interaction} interaction - interaction object
+   * @param {CommandInteraction} interaction - interaction object
    */
   async run(interaction) {
     try {
       if (interaction.isApplicationCommand()) {
         console.log("------");
 
-        console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.\nUser ID: ${interaction.user.id}, Command: ${interaction.commandName}\n`);
+        console.log(`${interaction.user.tag} in #${
+          interaction.channel.name
+        } triggered an interaction.\nUser ID: ${
+          interaction.user.id
+        }\nCommand: ${interaction.commandName} \nSubcommand: \n - Group: ${
+          interaction.options.getSubcommandGroup(false) || "N/A"
+        } \n - Name: ${interaction.options.getSubcommand(false) || "N/A"}`);
 
         console.log("---\nCommand Logs:");
 
@@ -37,8 +43,14 @@ export default new Event({
             .setDescription(`${interaction.user} in ${interaction.channel} triggered an interaction.`)
             .addFields([
               {
-                name: "**Command Name**",
+                name: "**Command**",
                 value: `${interaction.commandName}`
+              },
+              {
+                name: "**Subcommand**",
+                value: `Group: ${
+                  interaction.options.getSubcommandGroup(false) || "N/A"
+                }\nName: ${interaction.options.getSubcommand(false) || "N/A"}`
               },
               {
                 name: "**User ID**",
