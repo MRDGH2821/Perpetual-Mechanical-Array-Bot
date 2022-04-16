f0432b0835a8bf5edc9bdb2ab6adc008eea8a4ca
MRDGH2821 <mihirrabade@gmail.com>
2022-04-16 20:40:56 +0530

    feat: add pma base module

---
+++ pmaBaseModule/pmaBaseModule.js
@@ -0,0 +1,8 @@
+import { Module } from '@ruinguard/core';
+import esmImporter from '../lib/esmImporter.js';
+
+export default new Module({
+  commands: await esmImporter('./pmaBaseModule/commands'),
+  events: await esmImporter('./pmaBaseModule/events'),
+  name: 'Base PMA module',
+});
