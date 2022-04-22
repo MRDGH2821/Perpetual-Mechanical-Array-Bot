923407c902d1ef8eefe0f363db737a4b6102a5b0
MRDGH2821 <mihirrabade@gmail.com>
2022-04-22 22:47:11 +0530

    fix: add intents

--- pmaBaseModule.js
+++ pmaBaseModule.js
@@ -1,8 +1,9 @@
-import { Module } from '@ruinguard/core';
+import { Intents, Module } from '@ruinguard/core';
 import esmImporter from './lib/esmImporter.js';
 
 export default new Module({
   commands: await esmImporter('./pmaBaseModule/commands'),
   events: await esmImporter('./pmaBaseModule/events'),
   name: 'Base PMA module',
+  intents: new Intents([Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]),
 });
