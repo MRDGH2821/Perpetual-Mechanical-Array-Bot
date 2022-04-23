d863cbaa20e81471c0f752ed37664a551133916f
MRDGH2821 <mihirrabade@gmail.com>
2022-04-23 14:45:57 +0530

    feat: combine all auto-completions

---
+++ pmaBaseModule/events/mainAutoComplete.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+import { AutocompleteInteraction, Event } from '@ruinguard/core';
+import {
+  amcAutoComplete,
+  gmcAutoComplete
+} from '../../lib/TravelerTechnologies.js';
+
+export default new Event({
+  event: 'interactionCreate',
+  name: 'AMC Techs Auto Complete',
+  /**
+   * @async
+   * @function run
+   * @param {AutocompleteInteraction} interaction - interaction object
+   */
+  async run(interaction) {
+    if (interaction.isAutocomplete()) {
+      let responses = {};
+
+      if (interaction.commandName === 'amc') {
+        responses = await amcAutoComplete(interaction);
+      }
+      if (interaction.commandName === 'gmc') {
+        responses = await gmcAutoComplete(interaction);
+      }
+
+      await interaction.respond(responses);
+    }
+    // eslint-disable-next-line no-underscore-dangle
+    await interaction.client._onInteractionCreate(interaction);
+  },
+});
