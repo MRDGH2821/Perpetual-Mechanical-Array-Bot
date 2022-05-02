8a8da8e5cee40985f2f3bd85a97f768714a54e4c
MRDGH2821 <mihirrabade@gmail.com>
2022-05-02 19:28:06 +0530

    chore: overwrite guild commands using rest

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { InteractionCommandClient } from 'detritus-client';
 import { GatewayIntents } from 'detritus-client-socket/lib/constants';
 import EnvConfig from './lib/EnvConfig';
-import ping from './pmaBaseModule/ping';
+import test from './pmaBaseModule/test';
 
 const bot = new InteractionCommandClient(EnvConfig.token as string, {
   gateway: {
@@ -9,8 +9,15 @@ const bot = new InteractionCommandClient(EnvConfig.token as string, {
   },
 });
 
-bot.addMultiple([ping]);
+bot.add(test);
 
 bot.run().then(() => {
   console.log('bot on');
+  bot.client.rest
+    .bulkOverwriteApplicationGuildCommands(
+      process.env.CLIENT_ID as string,
+      process.env.GUILD_ID as string,
+      [test],
+    )
+    .then(() => console.log('commands overwritten'));
 });
