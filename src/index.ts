48ff17e81432ba2b637071a33d08c025bb18578e
MRDGH2821 <mihirrabade@gmail.com>
2022-05-02 19:43:34 +0530

    chore: add async anon function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { InteractionCommandClient } from 'detritus-client';
 import { GatewayIntents } from 'detritus-client-socket/lib/constants';
 import EnvConfig from './lib/EnvConfig';
+import ping from './pmaBaseModule/ping';
 import test from './pmaBaseModule/test';
 
 const bot = new InteractionCommandClient(EnvConfig.token as string, {
@@ -9,15 +10,17 @@ const bot = new InteractionCommandClient(EnvConfig.token as string, {
   },
 });
 
-bot.add(test);
-
-bot.run().then(() => {
-  console.log('bot on');
-  bot.client.rest
-    .bulkOverwriteApplicationGuildCommands(
-      process.env.CLIENT_ID as string,
-      process.env.GUILD_ID as string,
-      [test],
-    )
-    .then(() => console.log('commands overwritten'));
-});
+(async () => {
+  await bot.add(test);
+  await bot.add(ping);
+  await bot.run().then(async () => {
+    console.log('bot on');
+    await bot.client.rest
+      .bulkOverwriteApplicationGuildCommands(
+        process.env.CLIENT_ID as string,
+        process.env.GUILD_ID as string,
+        [test, ping],
+      )
+      .then(() => console.log('commands overwritten'));
+  });
+})();
