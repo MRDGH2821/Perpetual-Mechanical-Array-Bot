7b97eff79a676c41e3239099668676772f9ae663
MRDGH2821 <mihirrabade@gmail.com>
2022-05-03 19:50:44 +0530

    fix: application commands not running
    Co-Authored-By: Leaf <59538202+AroLeaf@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,30 @@
-import { InteractionCommandClient } from 'detritus-client';
+import { ClusterClient, InteractionCommandClient } from 'detritus-client';
 import { GatewayIntents } from 'detritus-client-socket/lib/constants';
 import EnvConfig from './lib/EnvConfig';
 import ping from './pmaBaseModule/ping';
 import test from './pmaBaseModule/test';
 
-const bot = new InteractionCommandClient(EnvConfig.token as string, {
-  gateway: {
-    intents: [GatewayIntents.GUILDS, GatewayIntents.GUILD_MEMBERS, GatewayIntents.GUILD_MESSAGES],
-  },
-});
-
 (async () => {
+  const cluster = new ClusterClient(EnvConfig.token as string, {
+    gateway: {
+      intents: [GatewayIntents.GUILDS, GatewayIntents.GUILD_MEMBERS, GatewayIntents.GUILD_MESSAGES],
+    },
+  });
+
+  await cluster.run();
+
+  const shard = cluster.shards.first()!;
+
+  const bot = new InteractionCommandClient(cluster);
   try {
     await bot.add(test); // added await even if not required
     await bot.add(ping);
+
     await bot.run().then(async () => {
       console.log('bot on');
     });
   } catch (err) {
     console.log(err);
   }
+  console.log(await shard.rest.fetchApplicationCommands(shard.applicationId));
 })();
