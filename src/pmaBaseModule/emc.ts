490a1018fa894fbc190ed0ceb24ee84cafe68241
MRDGH2821 <mihirrabade@gmail.com>
2022-05-03 23:01:37 +0530

    feat: add gmc &emc commands

---
+++ src/pmaBaseModule/emc.ts
@@ -0,0 +1,60 @@
+import { ApplicationCommandOptionTypes } from 'detritus-client/lib/constants';
+import { InteractionCommand, ParsedArgs } from 'detritus-client/lib/interaction';
+import EnvConfig from '../lib/EnvConfig';
+import { EMC_TECHS } from '../lib/TravelerTechnologies';
+
+interface CommandArgs extends ParsedArgs {
+  techs?: string;
+}
+
+export default new InteractionCommand({
+  name: 'emc',
+  description: 'Electro Main Character',
+  global: false,
+  guildIds: [EnvConfig.guildId as string],
+  options: [
+
+    {
+      name: 'bellowing_thunder',
+      description: 'EMC Burst',
+      type: ApplicationCommandOptionTypes.SUB_COMMAND,
+      options: [
+        {
+          name: 'techs',
+          description: 'Technologies which power burst',
+          type: ApplicationCommandOptionTypes.STRING,
+          required: true,
+          onAutoComplete(ctx) {
+            const inputVal = ctx.value.toLowerCase();
+            const choiceArr = EMC_TECHS.BURST_TECHS;
+            const values = choiceArr.filter((tech) => tech.name.toLowerCase().includes(inputVal));
+
+            const choices = values.map((tech) => ({
+              name: tech.name,
+              value: tech.id,
+            }));
+
+            ctx.respond({ choices });
+          },
+        },
+      ],
+      async run(ctx, args: CommandArgs) {
+        const techId = args.techs;
+        const selectedTech = EMC_TECHS.BURST_TECHS.find((tech) => tech.id === techId);
+        ctx.editOrRespond({
+          content: `**${selectedTech?.name}**\n\n${selectedTech?.gif}`,
+        });
+      },
+    },
+    {
+      name: 'guide',
+      description: 'Guide on EMC',
+      type: ApplicationCommandOptionTypes.SUB_COMMAND,
+      async run(ctx) {
+        await ctx.editOrRespond({
+          content: 'https://keqingmains.com/electro-traveler/',
+        });
+      },
+    },
+  ],
+});
