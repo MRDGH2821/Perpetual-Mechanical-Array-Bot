9f05ef2588fe94efe0636e7d446e105cd8a9d53a
MRDGH2821 <mihirrabade@gmail.com>
2022-05-03 20:36:52 +0530

    wip: add autocomplete logic & response logic

--- src/pmaBaseModule/amc.ts
+++ src/pmaBaseModule/amc.ts
@@ -1,25 +1,47 @@
-import { InteractionCallbackTypes } from 'detritus-client/lib/constants';
+import { ApplicationCommandOptionTypes } from 'detritus-client/lib/constants';
 import { InteractionCommand } from 'detritus-client/lib/interaction';
+import EnvConfig from 'lib/EnvConfig';
+import { AMC_TECHS } from '../lib/TravelerTechnologies';
 
 export default new InteractionCommand({
   name: 'amc',
   description: 'Anemo Main Character',
   global: false,
-  guildIds: [process.env.GUILD_ID as string],
+  guildIds: [EnvConfig.guildId as string],
+
   options: [
     {
       name: 'palm_vortex',
       description: 'AMC Skill',
-      isSubCommand: true,
-      choices: [
+      type: ApplicationCommandOptionTypes.SUB_COMMAND,
+      options: [
         {
           name: 'techs',
+          description: 'Technologies which power skill',
+          type: ApplicationCommandOptionTypes.STRING,
+
+          async onAutoComplete(ctx) {
+            const choices: { name: string; value: string }[] = [];
+            AMC_TECHS.SKILL_TECHS.forEach((skill) => {
+              const dat = {
+                name: skill.name,
+                value: skill.id,
+              };
+              choices.push(dat);
+            });
+            await ctx.respond({ choices });
+          },
+
+          async run(ctx) {
+            const techId = ctx.options?.get('tech');
+            const skillTech = AMC_TECHS.SKILL_TECHS.find((skill) => skill.id === techId?.value)
+              || AMC_TECHS.SKILL_TECHS[0];
+            ctx.editOrRespond({
+              content: `**${skillTech.name || 'possibly undefined'}**\n\n${skillTech?.gif}`,
+            });
+          },
         },
       ],
-
-      async run(ctx, args) {
-        ctx.respond(InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE, 'test');
-      },
     },
   ],
 });
