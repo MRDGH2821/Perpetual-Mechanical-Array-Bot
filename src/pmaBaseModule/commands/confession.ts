a284b4083e22d5278f648d50259163a28c587d8d
MRDGH2821 <mihirrabade@gmail.com>
2022-05-06 18:25:15 +0530

    feat: add confession command

---
+++ src/pmaBaseModule/commands/confession.ts
@@ -0,0 +1,93 @@
+import { GatewayRawEvents } from 'detritus-client';
+import { ApplicationCommandOptionTypes, MessageFlags } from 'detritus-client/lib/constants';
+import { InteractionCommand } from 'detritus-client/lib/interaction';
+import { CHANNEL_IDS, COLORS, ROLE_IDS } from '../../lib/Constants';
+import EnvConfig from '../../lib/EnvConfig';
+
+export default new InteractionCommand({
+  name: 'confess',
+  description: 'Wanna confess something?',
+  global: false,
+  guildIds: [EnvConfig.guildId as string],
+  options: [
+    {
+      name: 'confession',
+      description: 'Enter your confession!',
+      required: true,
+      type: ApplicationCommandOptionTypes.STRING,
+    },
+    {
+      name: 'anonymous',
+      description: 'Post as Anonymous?',
+      type: ApplicationCommandOptionTypes.BOOLEAN,
+      default: false,
+    },
+    {
+      name: 'ping_archons',
+      description: 'Notify Archons?',
+      type: ApplicationCommandOptionTypes.BOOLEAN,
+      default: false,
+    },
+  ],
+  async run(ctx, args) {
+    const isAnon = args.anonymous;
+    const shouldPingArchons = args.ping_archons;
+
+    const confessChannel = ctx.guild?.channels.get(CHANNEL_IDS.CONFESSIONS);
+
+    const logsChannel = ctx.guild?.channels.get(CHANNEL_IDS.ARCHIVES);
+
+    const anonEmbed: GatewayRawEvents.RawMessageEmbed = {
+      title: '**A New confession!**',
+      author: {
+        name: 'Anonymous',
+      },
+      color: COLORS.EMBED_COLOR,
+      description: args.confession,
+      timestamp: new Date().toISOString(),
+    };
+
+    const confessEmbed: GatewayRawEvents.RawMessageEmbed = {
+      title: '**A New confession!**',
+      author: {
+        name: ctx.user.tag,
+        icon_url: ctx.user.avatarUrlFormat(),
+        url: ctx.user.jumpLink,
+      },
+      color: COLORS.EMBED_COLOR,
+      description: args.confession,
+      timestamp: new Date().toISOString(),
+      thumbnail: {
+        url: ctx.user.avatarUrl,
+      },
+    };
+
+    const pingText = shouldPingArchons ? `<@&${ROLE_IDS.ARCHONS}>` : ' ';
+
+    if (isAnon) {
+      await confessChannel?.createMessage({
+        embeds: [anonEmbed],
+        content: pingText,
+      });
+
+      await ctx.editOrRespond({
+        content: `Confession sent as Anonymous!\nCheck out <#${confessChannel?.id}>`,
+        flags: MessageFlags.EPHEMERAL,
+      });
+    } else {
+      confessChannel?.createMessage({
+        embeds: [confessEmbed],
+        content: pingText,
+      });
+
+      await ctx.editOrRespond({
+        content: `Confession sent!\nCheck out <#${confessChannel?.id}>`,
+        flags: MessageFlags.EPHEMERAL,
+      });
+    }
+
+    logsChannel?.createMessage({
+      embeds: [confessEmbed],
+    });
+  },
+});
