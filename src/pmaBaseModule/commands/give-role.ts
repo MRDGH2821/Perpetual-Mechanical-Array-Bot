2e6e2e703e61beb9c39b055dc88f8c4557b4bf74
MRDGH2821 <mihirrabade@gmail.com>
2022-05-12 15:10:38 +0530

    wip: add give multi role command

--- src/pmaBaseModule/commands/give-role.ts
+++ src/pmaBaseModule/commands/give-role.ts
@@ -4,9 +4,10 @@ import * as RoleCheck from '@pma-lib/RoleCheck';
 import { canGibRole } from '@pma-lib/StaffCheck';
 import { GiveRoleArgs } from '@pma-types/interfaces';
 import { RequestTypes } from 'detritus-client-rest';
-import { ApplicationCommandOptionTypes } from 'detritus-client/lib/constants';
+import { ApplicationCommandOptionTypes, MessageFlags } from 'detritus-client/lib/constants';
 import { InteractionCommand } from 'detritus-client/lib/interaction';
 import { Member } from 'detritus-client/lib/structures';
+import { ComponentActionRow } from 'detritus-client/lib/utils';
 
 export default new InteractionCommand({
   name: 'give-role',
@@ -99,6 +100,121 @@ export default new InteractionCommand({
 
         await finalMsg.reply({
           content: `>award ${args.user?.id} ${exp}`,
+        });
+      },
+    },
+    {
+      name: 'multi',
+      description: 'Give multiple roles!',
+      type: ApplicationCommandOptionTypes.SUB_COMMAND,
+      options: [
+        {
+          name: 'user',
+          description: 'Select user',
+          type: ApplicationCommandOptionTypes.USER,
+          required: true,
+        },
+      ],
+      async onBefore(ctx) {
+        const canGib = canGibRole(ctx.member as Member);
+
+        if (!canGib) {
+          await ctx.editOrRespond({
+            content: `You cannot give roles to anyone, not even to yourself ${Constants.EMOJIS.PepeKekPoint}`,
+            flags: MessageFlags.EPHEMERAL,
+          });
+          return false;
+        }
+        return true;
+      },
+
+      async run(ctx, args: GiveRoleArgs) {
+        const firstPage: RequestTypes.CreateChannelMessageEmbed = {
+          title: '**Select Roles**',
+          description: `Select Roles to give to @${args.user}. The amount of EXP will be calculated in end.`,
+        };
+        const target = args.user;
+        const rolesSelectMenu = new ComponentActionRow().addSelectMenu({
+          customId: 'role_select_menu',
+          minValues: 2,
+          options: [
+            {
+              description: 'Completed Spiral Abyss 36/36 & all Spiral abyss achievements',
+              emoji: '🌀',
+              label: ctx.guild?.roles.get(Constants.ROLE_IDS.ABYSSAL_CONQUEROR)?.name,
+              value: Constants.ROLE_IDS.ABYSSAL_CONQUEROR,
+            },
+            {
+              default: target?.roles.has(Constants.ROLE_IDS.REPUTATION.MONDSTADT),
+              description: '100% Map + Subregions + Achievements + Max Reputation',
+              emoji: '🕊️',
+              label: ctx.guild?.roles.get(Constants.ROLE_IDS.REPUTATION.MONDSTADT)?.name,
+              value: Constants.ROLE_IDS.REPUTATION.MONDSTADT,
+            },
+            {
+              default: target?.roles.has(Constants.ROLE_IDS.REPUTATION.LIYUE),
+              description: '100% Map + Subregions + Achievements + Max Reputation',
+              emoji: '⚖️',
+              label: ctx.guild?.roles.get(Constants.ROLE_IDS.REPUTATION.LIYUE)?.name,
+              value: Constants.ROLE_IDS.REPUTATION.LIYUE,
+            },
+            {
+              default: target?.roles.has(Constants.ROLE_IDS.REPUTATION.INAZUMA),
+              description: '100% Map + Subregions + Achievements + Max Reputation',
+              emoji: '⛩️',
+              label: ctx.guild?.roles.get(Constants.ROLE_IDS.REPUTATION.INAZUMA)?.name,
+              value: Constants.ROLE_IDS.REPUTATION.INAZUMA,
+            },
+            {
+              description: 'Crowned their Anemo Traveler',
+              emoji: '🌪️',
+              label: ctx.guild?.roles.get(Constants.ROLE_IDS.CROWN.ANEMO)?.name,
+              value: Constants.ROLE_IDS.CROWN.ANEMO,
+            },
+            {
+              description: 'Crowned their Geo Traveler',
+              emoji: '🪨',
+              label: ctx.guild?.roles.get(Constants.ROLE_IDS.CROWN.GEO)?.name,
+              value: Constants.ROLE_IDS.CROWN.GEO,
+            },
+            {
+              description: 'Crowned their Electro Traveler',
+              emoji: '⚡',
+              label: ctx.guild?.roles.get(Constants.ROLE_IDS.CROWN.ELECTRO)?.name,
+              value: Constants.ROLE_IDS.CROWN.ELECTRO,
+            },
+            {
+              default: target?.roles.has(Constants.ROLE_IDS.CROWN.NON_ELE),
+              description: 'Crowned their Unaligned Traveler',
+              emoji: '👑',
+              label: ctx.guild?.roles.get(Constants.ROLE_IDS.CROWN.NON_ELE)?.name,
+              value: Constants.ROLE_IDS.CROWN.NON_ELE,
+            },
+            {
+              default: target?.roles.has(Constants.ROLE_IDS.WHALE),
+              description: 'Spent $1500, or have c6 5* chars or r5 5* weapons',
+              emoji: '💰',
+              label: ctx.guild?.roles.get(Constants.ROLE_IDS.WHALE)?.name,
+              value: Constants.ROLE_IDS.WHALE,
+            },
+          ].filter((option) => {
+            if (Object.values(Constants.ROLE_IDS.CROWN).includes(option.value)) {
+              return true;
+            }
+            if (option.value === Constants.ROLE_IDS.ABYSSAL_CONQUEROR) {
+              return true;
+            }
+            return !target?.roles.has(option.value);
+          }),
+
+          async run(menuCtx) {
+            console.log(menuCtx);
+          },
+        });
+
+        await ctx.editOrRespond({
+          embeds: [firstPage],
+          components: [rolesSelectMenu],
         });
       },
     },
