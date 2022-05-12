c0f342eedee962781714233e5edca1180ed247c8
MRDGH2821 <mihirrabade@gmail.com>
2022-05-12 13:52:27 +0530

    feat: add Role check & giverole arg interface

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,8 +1,25 @@
 import { Constants } from 'detritus-client';
+import { Snowflake } from 'detritus-client/lib/constants';
+import { ParsedArgs } from 'detritus-client/lib/interaction';
+import { Member } from 'detritus-client/lib/structures';
 
 export interface IEvent {
   event: Constants.ClientEvents;
   on?: boolean;
   once?: boolean;
   listener(payload?: any): any;
+}
+
+export interface AfterRoleCheck {
+  exp: number;
+  notes: string;
+}
+
+export interface AfterCrownRoleCheck extends AfterRoleCheck {
+  crownRole: Snowflake;
+}
+
+export interface GiveRoleArgs extends ParsedArgs {
+  user?: Member;
+  role?: Snowflake;
 }
