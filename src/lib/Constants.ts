11a17cf986a60dd8948d2a051ebfae4a22b5fa33
MRDGH2821 <mihirrabade@gmail.com>
2022-05-23 17:08:54 +0530

    feat: add icons & colors

--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -4,10 +4,34 @@ function formatEmoji(EmojiSnowflake: Snowflake) {
   return `<:_:${EmojiSnowflake}>`;
 }
 
+export const ICONS = {
+  COPIUM: 'https://cdn.discordapp.com/emojis/897176156057518130.webp?&quality=lossless',
+  VOID: 'https://cdn.discordapp.com/emojis/886587673408569394.png?v=1',
+  ANEMO: 'https://cdn.discordapp.com/emojis/803516622772895764.webp?&quality=lossless',
+  ELECTRO: 'https://cdn.discordapp.com/emojis/803516644923146260.webp?&quality=lossless',
+  GEO: 'https://cdn.discordapp.com/emojis/803516612430135326.webp?&quality=lossless',
+  PALM_VORTEX: 'https://cdn.discordapp.com/emojis/840965851199832087.png?v=1',
+  STARFELL_SWORD: 'https://cdn.discordapp.com/emojis/840965876370112532.png?v=1',
+  LIGHTENING_BLADE:
+    'https://cdn.discordapp.com/attachments/817208583988051999/886635086362071040/ElectroAether3.png',
+  SPIRAL_ABYSS: 'https://cdn.discordapp.com/emojis/806999511096361031.png?v=1',
+  CHECK_MARK:
+    'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/check-mark-button_2705.png',
+  CROSS_MARK:
+    'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/cross-mark_274c.png',
+};
+
 export const COLORS = {
   EMBED_COLOR: 0xe0d1bd,
   INVISIBLE: 0x2f3136,
   BLURPLE: Colors.BLURPLE,
+  ANEMO: 0x00ffcc,
+  GEO: 0xfce200,
+  ELECTRO: 0xa500ff,
+  NON_ELE: 0x01152d,
+  UNIVERSAL: 0xfffffd,
+  ERROR: 0xff0033,
+  SUCCESS: 0x00c455,
 };
 
 export const EMOJIS = {
