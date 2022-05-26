baa0ef73fa7bd1f7bafca3c3465bd33b6d901913
MRDGH2821 <mihirrabade@gmail.com>
2022-05-26 14:48:48 +0530

    feat: add random skill icon picker, rename link checker

--- src/lib/UtilityFunctions.ts
+++ src/lib/UtilityFunctions.ts
@@ -1,3 +1,5 @@
+import { CHANNEL_IDS, ICONS } from '@pma-lib/Constants';
+
 export function debug(obj: any) {
   return JSON.stringify(obj, null, 2);
 }
@@ -6,6 +8,36 @@ export function leafDebug(obj: any) {
   console.log(debug(obj));
 }
 
-export function leaderboardLinkCheck(link: string) {
-  return link.startsWith('https://discord.com/channels/803424731474034709/876121506680287263/');
+export function isLeaderboardLink(link: string) {
+  leafDebug(link.match(CHANNEL_IDS.SHOWCASE));
+  return link.match(CHANNEL_IDS.SHOWCASE)!.length > 0;
+
+  // return /876121506680287263/gm.test(link);
+}
+
+export function randomArrPick(array: any[]) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+export function randomSkillIcon(
+  element: 'anemo' | 'geo' | 'electro' | 'dendro' | 'hydro' | 'pyro' | 'cryo',
+) {
+  switch (element) {
+    case 'anemo': {
+      const icons = [ICONS.ANEMO, ICONS.PALM_VORTEX_AETHER];
+      return randomArrPick(icons);
+    }
+    case 'geo': {
+      const icons = [ICONS.GEO, ICONS.STARFELL_SWORD_LUMINE];
+      return randomArrPick(icons);
+    }
+    case 'electro': {
+      const icons = [ICONS.ELECTRO, ICONS.LIGHTENING_BLADE_AETHER];
+      return randomArrPick(icons);
+    }
+    default: {
+      const icons = [ICONS.VOID, ICONS.COPIUM];
+      return randomArrPick(icons);
+    }
+  }
 }
