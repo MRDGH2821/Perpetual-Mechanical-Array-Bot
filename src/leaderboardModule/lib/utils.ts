e1d589bc8107e8170efbb5a21843f9f053557e5e
MRDGH2821 <mihirrabade@gmail.com>
2022-05-29 22:45:48 +0530

    wip: add category props

---
+++ src/leaderboardModule/lib/utils.ts
@@ -0,0 +1,6 @@
+import { ElementCategories } from '@pma-types/enums';
+import { Colors } from 'detritus-client/lib/constants';
+
+export function categoryProps(dmgCategory: ElementCategories): {
+  color: COLORS;
+} {}
