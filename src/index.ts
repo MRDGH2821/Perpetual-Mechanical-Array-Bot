685ef351dabb585bc7c027ca971f7f1c9c54d2ab
MRDGH2821 <mihirrabade@gmail.com>
2022-05-31 20:20:17 +0530

    feat: wrap into try catch block

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,54 @@
-import EnvConfig from '@pma-lib/EnvConfig';
-import esmImporter from '@pma-lib/esmImporter';
-import { leafDebug, PMAEventHandler } from '@pma-lib/UtilityFunctions';
-import { IEvent } from '@pma-types/interfaces';
+import { IEvent } from '@bot-types/interfaces';
+import EnvConfig from '@lib/EnvConfig';
+import esmImporter from '@lib/esmImporter';
+import { Debugging, PMAEventHandler } from '@lib/Utilities';
 import { ClusterClient, InteractionCommandClient } from 'detritus-client';
 import { GatewayIntents } from 'detritus-client-socket/lib/constants';
 
-(async () => {
-  const pmaEvents: Array<IEvent> = await esmImporter('./src/pmaBaseModule/events');
-  const leaderboardEvents: Array<IEvent> = await esmImporter('./src/leaderboardModule/events/');
+try {
+  (async () => {
+    const pmaEvents: Array<IEvent> = await esmImporter('./src/pmaBaseModule/events');
+    const leaderboardEvents: Array<IEvent> = await esmImporter('./src/leaderboardModule/events/');
 
-  const botEvents = [pmaEvents, leaderboardEvents].flat();
+    const botEvents = [pmaEvents, leaderboardEvents].flat();
 
-  const clusterBot = new ClusterClient(EnvConfig.token as string, {
-    gateway: {
-      intents: [
-        GatewayIntents.GUILDS,
-        GatewayIntents.GUILD_MEMBERS,
-        GatewayIntents.GUILD_MESSAGES,
-        GatewayIntents.GUILD_EMOJIS,
-        GatewayIntents.GUILD_MESSAGE_REACTIONS,
-        GatewayIntents.GUILD_MESSAGE_TYPING,
-      ],
-    },
-    cache: {
-      members: { enabled: true, limit: 500 },
-    },
-  });
+    const clusterBot = new ClusterClient(EnvConfig.token as string, {
+      gateway: {
+        intents: [
+          GatewayIntents.GUILDS,
+          GatewayIntents.GUILD_MEMBERS,
+          GatewayIntents.GUILD_MESSAGES,
+          GatewayIntents.GUILD_EMOJIS,
+          GatewayIntents.GUILD_MESSAGE_REACTIONS,
+          GatewayIntents.GUILD_MESSAGE_TYPING,
+        ],
+      },
+      cache: {
+        members: { enabled: true, limit: 500 },
+      },
+    });
 
-  botEvents.forEach((pmaEvent) => {
-    if (pmaEvent.once) {
-      clusterBot.once(pmaEvent.event, pmaEvent.listener);
-    } else {
-      clusterBot.subscribe(pmaEvent.event, pmaEvent.listener);
-      PMAEventHandler.on(pmaEvent.event, pmaEvent.listener);
-    }
-  });
+    botEvents.forEach((pmaEvent) => {
+      if (pmaEvent.once) {
+        clusterBot.once(pmaEvent.event, pmaEvent.listener);
+      } else {
+        clusterBot.subscribe(pmaEvent.event, pmaEvent.listener);
+        PMAEventHandler.on(pmaEvent.event, pmaEvent.listener);
+      }
+    });
 
-  await clusterBot.run();
+    await clusterBot.run();
 
-  const interactionBot = new InteractionCommandClient(clusterBot);
+    const interactionBot = new InteractionCommandClient(clusterBot);
 
-  await interactionBot.addMultipleIn('./pmaBaseModule');
-  await interactionBot.addMultipleIn('./leaderboardModule/commands/');
+    await interactionBot.addMultipleIn('./pmaBaseModule');
+    await interactionBot.addMultipleIn('./leaderboardModule/commands/');
 
-  await interactionBot.run().catch((err) => {
-    console.error(err);
-    leafDebug(err);
-  });
-})();
+    await interactionBot.run().catch((err) => {
+      console.error(err);
+      Debugging.leafDebug(err);
+    });
+  })();
+} catch (error) {
+  Debugging.leafDebug(error);
+}
