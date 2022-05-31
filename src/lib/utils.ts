64f26ba068af559c1e3c1baaeae7b0d6eaf9889e
MRDGH2821 <mihirrabade@gmail.com>
2022-05-31 19:56:47 +0530

    feat: merge utilities into 1 file

--- src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import { ElementCategories } from '@pma-types/enums';
-import { Colors } from 'detritus-client/lib/constants';
-
-export function categoryProps(dmgCategory: ElementCategories): {
-  color: COLORS;
-} {}
