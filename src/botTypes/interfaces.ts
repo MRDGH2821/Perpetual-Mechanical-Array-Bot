7665fb4d0ce0a38b0c65f95c508401dc54ffd7fc
MRDGH2821 <mihirrabade@gmail.com>
2022-06-01 22:09:34 +0530

    feat: add nadeko embed & echo command interfaces

--- src/botTypes/interfaces.ts
+++ src/botTypes/interfaces.ts
@@ -3,7 +3,9 @@ import { Constants } from 'detritus-client';
 import { RequestTypes } from 'detritus-client-rest';
 import { Snowflake } from 'detritus-client/lib/constants';
 import { ParsedArgs } from 'detritus-client/lib/interaction';
-import { Member, User } from 'detritus-client/lib/structures';
+import {
+  Channel, InteractionEditOrRespond, Member, User,
+} from 'detritus-client/lib/structures';
 
 export interface IEvent {
   event: Constants.ClientEvents | string;
@@ -34,4 +36,38 @@ export interface LeaderBoardArgs extends ParsedArgs {
   group_type?: 'solo' | 'open';
   score?: number;
   proof_link?: string;
+}
+
+export interface EchoArgs extends ParsedArgs {
+  channel?: Channel;
+  text?: string;
+  embed?: string;
+  nadeko_json?: string;
+}
+
+export interface NadekoParseResult {
+  content: RequestTypes.CreateMessage['content'] | InteractionEditOrRespond['content'];
+  embeds: SimpleEmbed[];
+}
+
+export interface NadekoEmbed {
+  title?: string;
+  url?: string;
+  description?: string;
+  author?: {
+    name?: 'string';
+    icon_url?: 'string';
+  };
+  color?: string;
+  footer?: {
+    text?: string;
+    icon_url?: string;
+  };
+  thumbnail?: string;
+  image?: string;
+  fields?: SimpleEmbed['fields'];
+}
+export interface NadekoContent {
+  content?: string;
+  embeds?: NadekoEmbed[];
 }
