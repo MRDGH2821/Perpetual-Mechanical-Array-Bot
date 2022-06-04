7e57b7b143f27699c40e45d876c7139ce2268c1d
MRDGH2821 <mihirrabade@gmail.com>
2022-06-04 23:40:12 +0530

    fix: fix type checking

--- src/lib/BotClientExtracted.ts
+++ src/lib/BotClientExtracted.ts
@@ -9,7 +9,7 @@ export function setRestClient(RClient: RestClient) {
 }
 
 export function getRestClient(): RestClient {
-  if (typeof restClients[0] !== typeof RestClient) {
+  if (!restClients.length) {
     throw new Error('Rest client not initialised, use setRestClient first.');
   } else {
     return restClients[0];
@@ -24,7 +24,7 @@ export function setShardClient(SClient: ShardClient) {
 }
 
 export function getShardClient(): ShardClient {
-  if (typeof shardClients[0] !== typeof ShardClient) {
+  if (!shardClients.length) {
     throw new Error('Shard client not initialised, use setShardClient first.');
   } else {
     return shardClients[0];
