ae598258804a744ddcf819206d843d73804cbef5
MRDGH2821 <mihirrabade@gmail.com>
2022-06-04 22:03:23 +0530

    wip: get webhook from rest client

--- src/leaderboardModule/events/leaderboardUpdate.ts
+++ src/leaderboardModule/events/leaderboardUpdate.ts
@@ -1,13 +1,16 @@
 import { RestClient } from 'detritus-client/lib/rest';
+import { Webhook } from 'detritus-client/lib/structures';
 import BotEvent from '../../lib/BotEvent';
 import db from '../../lib/Firestore';
 import { showcaseLeaderboardGenerate } from '../../lib/leaderboardCacheManager';
-import { getRestClient } from '../../lib/RestClientExtracted';
+import { LeaderboardUpdateEventArgs } from '../../botTypes/types';
+import EnvConfig from '../../lib/EnvConfig';
+import { Debugging } from '../../lib/Utilities';
 
 export default new BotEvent({
   event: 'leaderboardUpdate',
   on: true,
-  async listener(RClient: RestClient = getRestClient()) {
+  async listener(args: LeaderboardUpdateEventArgs) {
     const anemoSkillBoard = await showcaseLeaderboardGenerate('anemo-dmg-skill');
     const geoSkillBoard = await showcaseLeaderboardGenerate('geo-dmg-skill');
     const electroSkillBoard = await showcaseLeaderboardGenerate('electro-dmg-skill');
@@ -21,9 +24,26 @@ export default new BotEvent({
     const uniMsg = (await leaderboardDB.doc('uni-dmg-skill').get()).data();
     const webhookMsg = (await leaderboardDB.doc('webhook').get()).data();
 
-    const leaderboardHook = await RClient.fetchWebhook(webhookMsg?.webhookID);
+    let leaderboardHook: Webhook;
+    if (args.webhook !== undefined) {
+      leaderboardHook = args.webhook;
+    } else if (args.SClient !== undefined) {
+      const guild = await args.SClient.guilds.find((guild) => guild.id === EnvConfig.guildId);
+      try {
+        const channel = await guild?.textChannels.find(
+          (channel) => channel.id === webhookMsg.channelID,
+        );
+        leaderboardHook = await (
+          await channel?.fetchWebhooks()
+        ).find((webhook) => webhook.id === webhookMsg.webhookID);
+      } catch (error) {
+        Debugging.leafDebug(error, true);
+      }
+    }
 
-    leaderboardHook.editMessage(anemoMsg?.messageID, { embed: anemoSkillBoard });
+    leaderboardHook.editMessage(anemoMsg?.messageID, {
+      embed: anemoSkillBoard,
+    });
     leaderboardHook.editMessage(geoMsg?.messageID, { embed: geoSkillBoard });
     leaderboardHook.editMessage(electroMsg?.messageID, { embed: electroSkillBoard });
     leaderboardHook.editMessage(uniMsg?.messageID, { embed: uniSkillBoard });
