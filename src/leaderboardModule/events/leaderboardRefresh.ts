efa81e49a69630e610a8353d3739e59d875a07e3
MRDGH2821 <mihirrabade@gmail.com>
2022-06-05 15:41:11 +0530

    feat: use leaderboard cache getter, log when caching done

--- src/leaderboardModule/events/leaderboardRefresh.ts
+++ src/leaderboardModule/events/leaderboardRefresh.ts
@@ -1,6 +1,6 @@
 import { RestClient } from 'detritus-client/lib/rest';
 import BotEvent from '../../lib/BotEvent';
-import { leaderboardCache, setLeaderboardData } from '../../lib/leaderboardCacheManager';
+import { getLBCacheObject, setLeaderboardData } from '../../lib/leaderboardCacheManager';
 import { getRestClient, getShardClient } from '../../lib/BotClientExtracted';
 import { Debugging, PMAEventHandler } from '../../lib/Utilities';
 import { LeaderboardUpdateEventArgs } from '../../botTypes/types';
@@ -9,7 +9,7 @@ export default new BotEvent({
   event: 'leaderboardRefresh',
   on: true,
   async listener(RClient: RestClient = getRestClient()) {
-    const LCache = leaderboardCache;
+    const LCache = getLBCacheObject();
     process.env.LEADERBOARD = 'false';
     console.log('Leaderboard Refresh Initiated');
 
@@ -22,12 +22,12 @@ export default new BotEvent({
         collection: LCache.anemo.skill.open,
         dmgCategory: 'anemo-dmg-skill',
         typeCategory: 'open',
-      }),
+      }).then(() => console.log('Anemo open done')),
       setLeaderboardData({
         collection: LCache.anemo.skill.solo,
         dmgCategory: 'anemo-dmg-skill',
         typeCategory: 'solo',
-      }),
+      }).then(() => console.log('Anemo solo done')),
     );
 
     // Geo Refresh
@@ -36,12 +36,12 @@ export default new BotEvent({
         collection: LCache.geo.skill.open,
         dmgCategory: 'geo-dmg-skill',
         typeCategory: 'open',
-      }),
+      }).then(() => console.log('Geo open done')),
       setLeaderboardData({
         collection: LCache.geo.skill.solo,
         dmgCategory: 'geo-dmg-skill',
         typeCategory: 'solo',
-      }),
+      }).then(() => console.log('Geo solo done')),
     );
 
     // Electro Refresh
@@ -50,12 +50,12 @@ export default new BotEvent({
         collection: LCache.electro.skill.open,
         dmgCategory: 'electro-dmg-skill',
         typeCategory: 'open',
-      }),
+      }).then(() => console.log('Electro open done')),
       setLeaderboardData({
         collection: LCache.electro.skill.solo,
         dmgCategory: 'electro-dmg-skill',
         typeCategory: 'solo',
-      }),
+      }).then(() => console.log('Electro solo done')),
     );
 
     // Universal n5 Refresh
@@ -64,18 +64,19 @@ export default new BotEvent({
         collection: LCache.uni.n5.open,
         dmgCategory: 'uni-dmg-n5',
         typeCategory: 'open',
-      }),
+      }).then(() => console.log('Uni open done')),
       setLeaderboardData({
         collection: LCache.uni.n5.solo,
         dmgCategory: 'uni-dmg-n5',
         typeCategory: 'solo',
-      }),
+      }).then(() => console.log('Uni solo done')),
     );
 
     await Promise.all(promises).then(() => {
       process.env.LEADERBOARD = 'true';
       console.log('Leaderboard Refresh Complete');
-      Debugging.leafDebug(leaderboardCache, true);
+      Debugging.leafDebug(LCache, true);
+
       console.log('Sending leaderboard update request');
 
       PMAEventHandler.emit('leaderboardUpdate', <LeaderboardUpdateEventArgs>{
