a530783d39a304cbf36b17e48eb1f1280fcf75f9
MRDGH2821 <mihirrabade@gmail.com>
2022-06-08 15:27:30 +0530

    feat: Add refresh, view mini & setup command

--- src/hallOfFameModule/commands/hall-of-fame.ts
+++ src/hallOfFameModule/commands/hall-of-fame.ts
@@ -1,14 +1,20 @@
 import {
   ApplicationCommandOptionTypes,
+  ChannelTypes,
   MessageComponentButtonStyles,
   MessageFlags,
 } from 'detritus-client/lib/constants';
 import { InteractionCommand } from 'detritus-client/lib/interaction';
+import { Channel } from 'detritus-client/lib/structures';
 import { ComponentActionRow } from 'detritus-client/lib/utils';
 import { ELEMENTS } from '../../botTypes/types';
 import EnvConfig from '../../lib/EnvConfig';
-import { hallOfFameViewGenerate, isHoFRefreshComplete } from '../../lib/hallOfFameCacheManager';
-import { getAbyssQuote } from '../../lib/Utilities';
+import {
+  hallOfFameViewGenerate,
+  isHoFRefreshComplete,
+  showcaseHallOfFameGenerate,
+} from '../../lib/hallOfFameCacheManager';
+import { getAbyssQuote, PMAEventHandler, StaffCheck } from '../../lib/Utilities';
 
 export default new InteractionCommand({
   name: 'hall-of-fame',
@@ -45,18 +51,16 @@ export default new InteractionCommand({
             },
           ],
         },
-        {
-          name: 'crown_quantity',
-          description: 'Select Crown Quantity',
-          type: ApplicationCommandOptionTypes.STRING,
-          required: true,
-          choices: [
-            { name: 'One (1)', value: 'one' },
-            { name: 'Two (2)', value: 'two' },
-            { name: 'Three (3)', value: 'three' },
-          ],
-        },
       ],
+      onBeforeRun(ctx) {
+        if (!isHoFRefreshComplete()) {
+          ctx.editOrRespond({
+            content: 'Refresh is ongoing, please wait for a while before using this command',
+            flags: MessageFlags.EPHEMERAL,
+          });
+        }
+        return isHoFRefreshComplete();
+      },
       async run(ctx, args) {
         let qty = args.crown_quantity;
         if (args.element === 'unaligned') {
@@ -115,14 +119,140 @@ export default new InteractionCommand({
         });
       },
     },
+    {
+      name: 'refresh',
+      description: 'Refreshes Hall Of Fame cache & optionally updates Hall Of Fame channel',
+      type: ApplicationCommandOptionTypes.SUB_COMMAND,
+      options: [
+        {
+          name: 'update_hall_of_fame',
+          description: 'Should update Hall Of Fame after cache refresh? (default False)',
+          type: ApplicationCommandOptionTypes.BOOLEAN,
+          default: false,
+        },
+      ],
+      onBeforeRun(ctx) {
+        if (!StaffCheck.isStaff(ctx.member!)) {
+          ctx.editOrRespond({
+            content: 'Only mods can change initiate a refresh',
+            flags: MessageFlags.EPHEMERAL,
+          });
+        }
+
+        if (!isHoFRefreshComplete()) {
+          ctx.editOrRespond({
+            content: 'Refresh is ongoing, please wait for a while before using this command',
+            flags: MessageFlags.EPHEMERAL,
+          });
+        }
+        return isHoFRefreshComplete() && StaffCheck.isStaff(ctx.member!);
+      },
+      async run(ctx, args) {
+        PMAEventHandler.emit('hallOfFameRefresh', args.update_hall_of_fame);
+
+        ctx.editOrRespond({
+          content: `Refresh initiated, please wait for a while before using this command\nWill update Leaderboard? \`${args.update_hall_of_fame}\``,
+        });
+      },
+    },
+    {
+      name: 'setup',
+      description: 'Select channel where hall of fame updates will come',
+      type: ApplicationCommandOptionTypes.SUB_COMMAND,
+      options: [
+        {
+          name: 'channel',
+          description: 'Select channel where hall of fame updates wil come',
+          type: ApplicationCommandOptionTypes.CHANNEL,
+          required: true,
+          channelTypes: [ChannelTypes.GUILD_TEXT],
+        },
+      ],
+      onBeforeRun(ctx) {
+        if (!StaffCheck.isStaff(ctx.member!)) {
+          ctx.editOrRespond({
+            content: 'Only mods can change hall of fame channel',
+            flags: MessageFlags.EPHEMERAL,
+          });
+        }
+
+        if (!isHoFRefreshComplete()) {
+          ctx.editOrRespond({
+            content: 'Please wait before using this command, refresh is not complete',
+            flags: MessageFlags.EPHEMERAL,
+          });
+        }
+
+        return StaffCheck.isStaff(ctx.member!) && isHoFRefreshComplete();
+      },
+      async run(ctx, args) {
+        const setupChannel = args.channel as Channel;
+
+        await ctx.editOrRespond({
+          content: `Selected channel: ${setupChannel.mention} `,
+          flags: MessageFlags.EPHEMERAL,
+        });
+
+        PMAEventHandler.emit('hallOfFameChannelUpdate', setupChannel);
+      },
+    },
+    {
+      name: 'view_mini',
+      description: 'View individual hall of fame embed',
+      type: ApplicationCommandOptionTypes.SUB_COMMAND,
+      options: [
+        {
+          name: 'element',
+          description: 'Select Element',
+          type: ApplicationCommandOptionTypes.STRING,
+          required: true,
+          choices: <{ name: string; value: ELEMENTS }[]>[
+            {
+              name: 'Herrscher of Wind (Anemo)',
+              value: 'anemo',
+            },
+            {
+              name: 'Jūnzhǔ of Earth (Geo)',
+              value: 'geo',
+            },
+            {
+              name: "Ten'nō of Thunder (Electro)",
+              value: 'electro',
+            },
+            {
+              name: 'Arbitrator of Fate (Unaligned)',
+              value: 'unaligned',
+            },
+          ],
+        },
+        {
+          name: 'crown_quantity',
+          description: 'Select Crown Quantity',
+          type: ApplicationCommandOptionTypes.STRING,
+          required: true,
+          choices: [
+            { name: 'One (1)', value: 'one' },
+            { name: 'Two (2)', value: 'two' },
+            { name: 'Three (3)', value: 'three' },
+          ],
+        },
+      ],
+      onBeforeRun(ctx) {
+        if (!isHoFRefreshComplete()) {
+          ctx.editOrRespond({
+            content: 'Refresh is ongoing, please wait for a while before using this command',
+            flags: MessageFlags.EPHEMERAL,
+          });
+        }
+        return isHoFRefreshComplete();
+      },
+      async run(ctx, args) {
+        const embed = await showcaseHallOfFameGenerate(args.element);
+
+        await ctx.editOrRespond({
+          embed,
+        });
+      },
+    },
   ],
-  onBeforeRun(ctx) {
-    if (!isHoFRefreshComplete()) {
-      ctx.editOrRespond({
-        content: 'Refresh is ongoing, please wait for a while before using this command',
-        flags: MessageFlags.EPHEMERAL,
-      });
-    }
-    return isHoFRefreshComplete();
-  },
 });
