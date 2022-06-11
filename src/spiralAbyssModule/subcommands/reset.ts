7ed9c6d315fcdeb56a1c138683973cb9fa2f8cea
MRDGH2821 <mihirrabade@gmail.com>
2022-06-11 23:11:56 +0530

    feat: add more parameters & information

--- src/spiralAbyssModule/subcommands/reset.ts
+++ src/spiralAbyssModule/subcommands/reset.ts
@@ -1,15 +1,19 @@
 import {
   ApplicationCommandOptionTypes,
-  InteractionCallbackTypes,
   MessageComponentButtonStyles,
+  MessageFlags,
 } from 'detritus-client/lib/constants';
 import { InteractionCommandOptionOptions } from 'detritus-client/lib/interaction';
 import { Member } from 'detritus-client/lib/structures';
 import { ComponentActionRow } from 'detritus-client/lib/utils';
 import { SimpleEmbed } from '../../botTypes/interfaces';
-import { COLORS, ICONS, ROLE_IDS } from '../../lib/Constants';
+import { getShardClient } from '../../lib/BotClientExtracted';
+import {
+  ChannelIds, COLORS, ICONS, ROLE_IDS,
+} from '../../lib/Constants';
 import db from '../../lib/Firestore';
-import { Debugging, StaffCheck } from '../../lib/Utilities';
+import { isSARefreshComplete } from '../../lib/spiralAbyssCacheManager';
+import { Debugging, PMAEventHandler, StaffCheck } from '../../lib/Utilities';
 
 const reset: InteractionCommandOptionOptions = {
   name: 'reset',
@@ -28,14 +32,32 @@ const reset: InteractionCommandOptionOptions = {
       type: ApplicationCommandOptionTypes.BOOLEAN,
       default: true,
     },
+    {
+      name: 'delete_cache',
+      description: 'Deletes spiral abyss cache (default True)',
+      type: ApplicationCommandOptionTypes.BOOLEAN,
+      default: true,
+    },
+    {
+      name: 'clear_database',
+      description: 'Deletes spiral abyss database entries (default True)',
+      type: ApplicationCommandOptionTypes.BOOLEAN,
+      default: true,
+    },
   ],
-  onBeforeRun(ctx) {
-    return StaffCheck.isCtxStaff(ctx, true);
+  async onBeforeRun(ctx) {
+    if (!isSARefreshComplete()) {
+      ctx.editOrRespond({
+        content: 'Please wait before using this command, refresh is not complete',
+        flags: MessageFlags.EPHEMERAL,
+      });
+    }
+    return (await StaffCheck.isCtxStaff(ctx, true)) && isSARefreshComplete();
   },
   async run(ctx, args) {
     const verifyEmb: SimpleEmbed = {
       title: '**Are you sure?**',
-      description: `Performing This action will result in the following -\n 1. Spiral Abyss cache + database entries will be cleared\n 2.Will publish names: \`${args.publish_names}\`\n 3. Will remove roles: \`${args.remove_roles}\``,
+      description: `Performing This action will result in the following -\n 1. Will delete Spiral Abyss cache \`${args.delete_cache}\` \n 2. Will clear database entries \`${args.clear_database}\` \n 3. Will publish names: \`${args.publish_names}\`\n 4. Will remove roles: \`${args.remove_roles}\``,
       color: COLORS.EMBED_COLOR,
       thumbnail: {
         url: '',
@@ -48,21 +70,40 @@ const reset: InteractionCommandOptionOptions = {
         emoji: '✅',
         style: MessageComponentButtonStyles.DANGER,
         async run(btnCtx) {
-          verifyEmb.fields = [
-            {
-              name: '**Done**',
-              value: 'Your command was successfully executed',
+          const successEmbed: SimpleEmbed = {
+            title: '**Success**',
+            description: ` 1. Spiral Abyss cache deleted: \`${args.delete_cache}\` \n 2. Database entries cleared: \`${args.clear_database}\` \n 3. Names Published: \`${args.publish_names}\`\n 4. Roles Removed: \`${args.remove_roles}\``,
+            color: COLORS.SUCCESS,
+            thumbnail: {
+              url: ICONS.CHECK_MARK,
             },
-          ];
-          verifyEmb.color = COLORS.SUCCESS;
-          verifyEmb.thumbnail!.url = ICONS.CHECK_MARK;
+            fields: [
+              {
+                name: '**Done**',
+                value: 'Your command was successfully executed',
+              },
+            ],
+          };
 
           const ids: { id: Member['user']['id'] }[] = [];
 
-          const dbIds: any = [];
+          const dbIds: any[] = [];
+
+          const abyssRole = ROLE_IDS.OTHERS.ANOTHER_TEST_SUBJECT;
+
+          // perform role backup
           await btnCtx.guild?.members
-            .filter((user) => user.roles.has(ROLE_IDS.OTHERS.ABYSSAL_CONQUEROR))
-            .forEach((member) => ids.push({ id: member.user.id }));
+            .filter((user) => user.roles.has(abyssRole))
+            .forEach((member) => {
+              ids.push({ id: member.user.id });
+
+              // remove role when true
+              if (args.remove_roles) {
+                member.removeRole(abyssRole);
+              }
+            });
+
+          // perform db backup
           await db
             .collection('spiral-abyss-current')
             .get()
@@ -72,21 +113,44 @@ const reset: InteractionCommandOptionOptions = {
               });
             });
 
-          await btnCtx.createResponse(InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE, {
-            embed: verifyEmb,
+          // publish names
+          if (args.publish_names === true) {
+            PMAEventHandler.emit('spiralAbyssPublish', {
+              SClient: getShardClient(),
+              deleteCache: args.delete_cache,
+              clearDB: args.clear_database,
+            });
+          }
+
+          PMAEventHandler.emit('spiralAbyssRefresh');
+          if (args.remove_roles === true) {
+            const announceEmb: SimpleEmbed = {
+              title: '**New Enemy Lineup!**',
+              description: `Enemy Lineup has changed which means Spiral abyss roles are up for grabs!\nSubmit in-game screenshot or Hoyolab profile link or Hoyolab screenshot at <#${ChannelIds.ROLE_APPLICATION}> as a proof to get the role!\n\n(~~Yes roles are removed~~)`,
+            };
+
+            ctx.channels.get(ChannelIds.SPIRAL_ABYSS)?.createMessage({
+              embed: announceEmb,
+            });
+          }
+
+          await btnCtx.editOrRespond({
+            embed: successEmbed,
             files: [
               {
-                description: 'IDs of people who had spiral abyss role',
-                filename: 'member_ids_spiral_abyss.txt',
-                value: JSON.stringify(ids),
+                filename: 'db_ids_spiral_abyss.json',
+                value: JSON.stringify(dbIds),
               },
               {
-                description: 'IDs stored in database',
-                filename: 'db_ids_spiral_abyss.txt',
-                value: JSON.stringify(dbIds),
+                filename: 'member_ids_spiral_abyss.json',
+                value: JSON.stringify(ids),
               },
             ],
           });
+          console.log('DB Ids Length: ', dbIds.length);
+          console.log('DB Ids str len: ', JSON.stringify(dbIds).length);
+          console.log('Member Ids Length: ', ids.length);
+          console.log('Member Ids str len: ', JSON.stringify(ids).length);
         },
         onError(errCtx, err) {
           Debugging.leafDebug(err);
@@ -106,7 +170,7 @@ const reset: InteractionCommandOptionOptions = {
           });
           verifyEmb.color = COLORS.ERROR;
           verifyEmb.thumbnail!.url = ICONS.CROSS_MARK;
-          btnCtx.createResponse(InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE, {
+          btnCtx.editOrRespond({
             embed: verifyEmb,
           });
         },
