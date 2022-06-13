51e511b2b5e61203facd88ecb7103f6f7eeced94
MRDGH2821 <mihirrabade@gmail.com>
2022-06-13 16:19:13 +0530

    chore: use firebase emulator when in development

--- src/lib/Firestore.ts
+++ src/lib/Firestore.ts
@@ -2,28 +2,33 @@ import { applicationDefault, initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import fs from 'fs';
 import * as path from 'path';
+import './EnvConfig';
 
-const configs = fs
-  .readdirSync('./src/lib/firebase-service-acc/')
-  .filter((file) => file.endsWith('.json'));
+if (process.env.NODE_ENV !== 'development') {
+  const configs = fs
+    .readdirSync('./src/lib/firebase-service-acc/')
+    .filter((file) => file.endsWith('.json'));
 
-console.log(configs);
-configs.sort();
+  console.log(configs);
+  configs.sort();
 
-configs.filter((config) => {
-  const configPath = path.resolve(process.cwd(), __dirname, 'firebase-service-acc', config);
-  const configContents = JSON.parse(fs.readFileSync(configPath).toString());
-  // console.log(configContents);
-  if (configContents.type === 'service_account') {
-    process.env.GOOGLE_APPLICATION_CREDENTIALS = configPath;
-    return true;
-  }
-  return false;
-});
+  configs.filter((config) => {
+    const configPath = path.resolve(process.cwd(), __dirname, 'firebase-service-acc', config);
+    const configContents = JSON.parse(fs.readFileSync(configPath).toString());
+    // console.log(configContents);
+    if (configContents.type === 'service_account') {
+      process.env.GOOGLE_APPLICATION_CREDENTIALS = configPath;
+      return true;
+    }
+    return false;
+  });
 
-initializeApp({
-  credential: applicationDefault(),
-});
+  initializeApp({
+    credential: applicationDefault(),
+  });
+} else {
+  initializeApp();
+}
 const db = getFirestore();
 
 export default db;
