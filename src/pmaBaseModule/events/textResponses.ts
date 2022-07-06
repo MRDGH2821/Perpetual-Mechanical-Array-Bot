b212227c3a527d30c8183fb2843bf23e595e1708
MRDGH2821 <mihirrabade@gmail.com>
2022-07-06 15:38:44 +0530

    feat: add text responses

---
+++ src/pmaBaseModule/events/textResponses.ts
@@ -0,0 +1,18 @@
+import { GatewayClientEvents } from 'detritus-client';
+import { ClientEvents } from 'detritus-client/lib/constants';
+import BotEvent from '../../lib/BotEvent';
+
+export default new BotEvent({
+  event: ClientEvents.MESSAGE_CREATE,
+  async listener(payload: GatewayClientEvents.MessageCreate) {
+    const { message } = payload;
+    const msg = message.content;
+
+    if (/banhammer/gimu.test(msg) && message.author.id === '440081484855115776') {
+      message.reply({
+        content: 'Who we are banning today? :smirk:',
+        reference: true,
+      });
+    }
+  },
+});
