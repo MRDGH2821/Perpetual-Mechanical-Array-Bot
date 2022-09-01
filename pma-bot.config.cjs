58baf27db0c0a19928053dcfcfea6e8ea4d48882
MRDGH2821 <mihirrabade@gmail.com>
2022-09-01 16:01:38 +0530

    chore: modify pm2 options to match transpiled code

--- pma-bot.config.cjs
+++ pma-bot.config.cjs
@@ -5,14 +5,8 @@ module.exports = {
       env_dev: { NODE_ENV: 'dev' },
       env_prod: { NODE_ENV: 'prod' },
       name: 'PMA-Bot',
-      script: './src/index.js',
+      script: './out/index.js',
       watch: true,
-    },
-    {
-      autorestart: false,
-      name: 'Deploy PMA-Bot',
-      script: './src/deploy-check.cjs',
-      watch: false,
     },
   ],
 };
