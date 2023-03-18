c34c28eea969ea419cdb067321dcf7a03611d5c5
MRDGH2821 <mihirrabade@gmail.com>
2023-03-18 18:53:14 +0530

    feat: add color constants

--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -1,4 +1,20 @@
+import { Colors } from 'discord.js';
 import { join } from 'path';
 
 export const rootDir = join(__dirname, '..', '..');
 export const srcDir = join(rootDir, 'src');
+
+export enum COLORS {
+  EMBED_COLOR = 0xe0d1bd,
+  INVISIBLE = 0x2f3136,
+  BLURPLE = Colors.Blurple,
+  ANEMO = 0x00ffcc,
+  GEO = 0xfce200,
+  ELECTRO = 0xa500ff,
+  UNALIGNED = 0x01152d,
+  UNIVERSAL = 0xfffffd,
+  ERROR = 0xff0033,
+  SUCCESS = 0x00c455,
+  SPIRAL_ABYSS = 0x4d00f0,
+  DENDRO = 0x94fe00,
+}
