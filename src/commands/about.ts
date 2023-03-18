d421ca168004fd7bce18b3a34294785977985808
MRDGH2821 <mihirrabade@gmail.com>
2023-03-18 19:17:09 +0530

    refactor: remove builders, use short circuit to find owner

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -1,6 +1,7 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, userMention } from 'discord.js';
+import { ButtonStyle, ComponentType } from 'discord-api-types/v10';
+import { User, userMention } from 'discord.js';
 import { COLORS } from '../lib/Constants';
 
 const pkg = require('../../package.json');
@@ -16,31 +17,37 @@ const deps = Object.entries(pkg.dependencies)
 export default class UserCommand extends Command {
   public override registerApplicationCommands(registry: Command.Registry) {
     registry.registerChatInputCommand((builder) =>
-      builder //
-        .setName(this.name)
-        .setDescription(this.description),
+      builder.setName(this.name).setDescription(this.description),
     );
   }
 
   public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+    const owner: User | undefined =
+      interaction.client.application.owner instanceof User
+        ? interaction.client.application.owner
+        : interaction.client.application.owner?.owner?.user;
     return interaction.reply({
       embeds: [
         {
           title: '**About the bot**',
           color: COLORS.EMBED_COLOR,
           description: `Version ${pkg.version}\nModified by: ${
-            interaction.client.application.owner
+            owner || '*cannot be determined*'
           }\nCreated by: ${userMention('867811595266424852')}\n\nThe bot uses:\n${deps}`,
         },
       ],
       components: [
-        new ActionRowBuilder<ButtonBuilder>().addComponents(
-          new ButtonBuilder({
-            label: 'Source Code',
-            style: ButtonStyle.Link,
-            url: pkg.homepage,
-          }),
-        ),
+        {
+          type: ComponentType.ActionRow,
+          components: [
+            {
+              type: ComponentType.Button,
+              label: 'Source Code',
+              style: ButtonStyle.Link,
+              url: pkg.homepage,
+            },
+          ],
+        },
       ],
     });
   }
