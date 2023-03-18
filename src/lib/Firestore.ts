6332414b3d0a923cf134d73db132af808bc5abc3
MRDGH2821 <mihirrabade@gmail.com>
2023-03-18 19:52:26 +0530

    feat: create Firestore.ts

---
+++ src/lib/Firestore.ts
@@ -0,0 +1,66 @@
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import fs from 'fs';
+import * as path from 'path';
+import './EnvConfig';
+
+const basePath = path.resolve(process.cwd(), 'firebase-service-acc');
+if (process.env.NODE_ENV !== 'development') {
+  process.env.FIRESTORE_EMULATOR_HOST = '';
+  const configs = fs.readdirSync(basePath).filter((file) => file.endsWith('.json'));
+
+  console.log(configs);
+  configs.sort();
+
+  const validConfigs = configs.filter((config) => {
+    const configPath = path.resolve(basePath, config);
+    // console.log(configPath);
+    const configContents = JSON.parse(fs.readFileSync(configPath).toString());
+    // console.log(configContents);
+
+    if (configContents.type === 'service_account') {
+      return true;
+    }
+    return false;
+  });
+  const certPath = path.resolve(basePath, validConfigs[0]);
+  // console.log(certPath);
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = certPath;
+}
+initializeApp();
+const db = getFirestore();
+
+export default db;
+
+async function deleteQueryBatch(query: FirebaseFirestore.Query, resolve: Function) {
+  const snapshot = await query.get();
+
+  const batchSize = snapshot.size;
+  if (batchSize === 0) {
+    // When there are no documents left, we are done
+    resolve();
+    return;
+  }
+
+  // Delete documents in a batch
+  const batch = db.batch();
+  snapshot.docs.forEach((doc) => {
+    batch.delete(doc.ref);
+  });
+  await batch.commit();
+
+  // Recurse on the next process tick, to avoid
+  // exploding the stack.
+  process.nextTick(() => {
+    deleteQueryBatch(query, resolve);
+  });
+}
+
+export async function deleteCollection(collectionPath: string, batchSize: number = 10) {
+  const collectionRef = db.collection(collectionPath);
+  const query = collectionRef.orderBy('__name__').limit(batchSize);
+
+  return new Promise((resolve, reject) => {
+    deleteQueryBatch(query, resolve).catch(reject);
+  });
+}
