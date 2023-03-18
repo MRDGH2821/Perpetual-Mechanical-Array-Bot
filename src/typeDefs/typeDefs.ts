4d8826ee50670e758bafdb4ea4f5daf09aa41832
MRDGH2821 <mihirrabade@gmail.com>
2023-03-18 22:14:43 +0530

    chore: add more types

--- src/typeDefs/typeDefs.ts
+++ src/typeDefs/typeDefs.ts
@@ -1,3 +1,37 @@
 import type { ChatInputApplicationCommandData } from 'discord.js';
 
 export type JSONCmd = ChatInputApplicationCommandData;
+
+export type ELEMENTS =
+  | 'anemo'
+  | 'geo'
+  | 'electro'
+  | 'dendro'
+  | 'hydro'
+  | 'pyro'
+  | 'cryo'
+  | 'unaligned'
+  | 'uni';
+
+export type TravelerTechProp = {
+  shortName: string;
+  name: string;
+  element: ELEMENTS;
+  skill: {
+    name: string;
+    techs: {
+      gif: string;
+      id: string;
+      name: string;
+    }[];
+  };
+  burst: {
+    name: string;
+    techs: {
+      gif: string;
+      id: string;
+      name: string;
+    }[];
+  };
+  guide: string;
+};
