b81a8e9567e414e2bce00003600bf92bd2f0380d
MRDGH2821 <mihirrabade@gmail.com>
2023-03-19 15:29:25 +0530

    refactor: use props

--- src/baseBot/commands/amc.ts
+++ src/baseBot/commands/amc.ts
@@ -1,65 +1,30 @@
 import { Subcommand } from '@sapphire/plugin-subcommands';
 import EnvConfig from '../../lib/EnvConfig';
-import { AMC_PROPS } from '../lib/TravelerTechnologies';
-import type { JSONCmd } from '../../typeDefs/typeDefs';
+import { replyTech, travelerCmdProps } from '../lib/Utilities';
 
-const cmdDef: JSONCmd = {
-  name: 'amc',
-  description: AMC_PROPS.name,
-  options: [
-    {
-      type: 1,
-      name: 'guide',
-      description: 'Anemo Traveler Guide on Keqing Mains',
-    },
-    {
-      type: 1,
-      name: AMC_PROPS.burst.name,
-      description: 'AMC Burst techs',
-      options: [
-        {
-          type: 3,
-          name: 'tech',
-          description: 'Select tech',
-          required: true,
-        },
-      ],
-    },
-    {
-      type: 1,
-      name: AMC_PROPS.skill.name,
-      description: 'AMC Skill techs',
-      options: [
-        {
-          type: 3,
-          name: 'tech',
-          description: 'Select tech',
-          required: true,
-        },
-      ],
-    },
-  ],
-};
+const cmdProps = travelerCmdProps('anemo');
+const cmdDef = cmdProps.commandDefinition;
+const { prop } = cmdProps;
 export default class UserCommand extends Subcommand {
   public constructor(context: Subcommand.Context, options: Subcommand.Options) {
     super(context, {
       ...options,
       name: cmdDef.name,
       description: cmdDef.description,
-
       subcommands: [
         {
           name: cmdDef.options![0].name,
           type: 'method',
+
           chatInputRun(interaction) {
-            return interaction.reply({ content: AMC_PROPS.guide });
+            return interaction.reply({ content: prop.guide });
           },
         },
         {
           name: cmdDef.options![1].name,
           type: 'method',
           chatInputRun(interaction) {
-            return interaction.reply({ content: 'Hello this is cmd2!' });
+            return replyTech(interaction, prop.element, 'burst');
           },
         },
         {
@@ -67,7 +32,7 @@ export default class UserCommand extends Subcommand {
           type: 'method',
 
           chatInputRun(interaction) {
-            return interaction.reply({ content: 'Hello this is cmd3!' });
+            return replyTech(interaction, prop.element, 'skill');
           },
         },
       ],
