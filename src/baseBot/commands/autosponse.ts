b157689101c16905fed872bbc058787275a06273
Mihir <mihirrabade@gmail.com>
2023-03-27 10:14:17 +0000

    feat: add new command autoresponse

---
+++ src/baseBot/commands/autosponse.ts
@@ -0,0 +1,127 @@
+import { Subcommand } from '@sapphire/plugin-subcommands';
+import { Time } from '@sapphire/time-utilities';
+import {
+  ApplicationCommandOptionType,
+  ApplicationCommandType,
+  MessageFlags,
+  time,
+} from 'discord.js';
+import EnvConfig from '../../lib/EnvConfig';
+import type { JSONCmd } from '../../typeDefs/typeDefs';
+
+const autoresponseChoices = [
+  {
+    name: 'TikTok',
+    value: 'TIKTOK',
+  },
+  {
+    name: 'Leaks',
+    value: 'LEAKS',
+  },
+  {
+    name: 'Fbi',
+    value: 'FBI',
+  },
+  {
+    name: 'Yoyoverse',
+    value: 'YOYOVERSE',
+  },
+];
+
+const cmdDef: JSONCmd = {
+  name: 'autoresponse',
+  description: 'Autoresponse settings',
+  type: ApplicationCommandType.ChatInput,
+  options: [
+    {
+      name: 'disable',
+      description: 'Disables an Auto response',
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: 'name',
+          description: 'Select which auto response to disable',
+          type: ApplicationCommandOptionType.String,
+          choices: autoresponseChoices,
+        },
+        {
+          name: 'duration',
+          description: 'For how many hours should this be disabled? (Default 24 hours)',
+          type: ApplicationCommandOptionType.Integer,
+          min_value: 0,
+          minValue: 0,
+          required: false,
+        },
+      ],
+    },
+    {
+      name: 'enable',
+      description: 'Enables an Auto response',
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: 'name',
+          description: 'Select which auto response to enable',
+          type: ApplicationCommandOptionType.String,
+          choices: autoresponseChoices,
+        },
+      ],
+    },
+  ],
+};
+
+export default class UserCommand extends Subcommand {
+  public constructor(context: Subcommand.Context, options: Subcommand.Options) {
+    super(context, {
+      ...options,
+      name: cmdDef.name,
+      description: cmdDef.description,
+      preconditions: ['ModOnly'],
+      subcommands: [
+        {
+          name: cmdDef.options![0].name,
+          type: 'method',
+          chatInputRun: 'chatRun',
+        },
+        {
+          name: cmdDef.options![1].name,
+          type: 'method',
+          chatInputRun: 'chatRun',
+        },
+      ],
+    });
+  }
+
+  public override registerApplicationCommands(registry: Subcommand.Registry) {
+    registry.registerChatInputCommand(cmdDef, {
+      guildIds: [EnvConfig.guildId],
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public chatRun(interaction: Subcommand.ChatInputCommandInteraction) {
+    const name = interaction.options.getString('name', true);
+    const hours = interaction.options.getInteger('duration') || 24;
+    const subCommand = interaction.options.getSubcommand(true);
+    function isToDisable() {
+      return subCommand.toLowerCase().includes('disable');
+    }
+    process.env[`AUTORESPONSE_${name}`] = String(isToDisable());
+
+    const content = isToDisable()
+      ? `${name} is now disabled for ${hours}h.\nIt will be enabled in ${time(
+        new Date(hours * Time.Hour + Date.now()),
+        'R',
+      )} \n\nDo note that if the bot restarts, it will be enabled again irrespective of given duration`
+      : `\`${name} autoresponse is now enabled.\``;
+
+    interaction.reply({
+      content,
+      flags: MessageFlags.Ephemeral,
+    });
+
+    setTimeout(() => {
+      process.env[`AUTORESPONSE_${name}`] = 'true';
+    }, hours * Time.Hour);
+  }
+}
