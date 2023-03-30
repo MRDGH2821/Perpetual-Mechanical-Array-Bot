29789e919bc864a5e30887db10e8e7493f446946
Mihir <mihirrabade@gmail.com>
2023-03-30 08:57:11 +0000

    feat: add cool down manager

---
+++ src/lib/CoolDownManager.ts
@@ -0,0 +1,27 @@
+/*
+Original Author - Aroleaf
+https://github.com/Khaenri-ah/ruinguard-core/blob/main/src/CooldownManager.ts
+*/
+
+import { Collection } from 'discord.js';
+
+export default class CoolDownManager {
+  coolDown: number;
+
+  coolDowns: Collection<string, number>;
+
+  constructor(coolDown = 0) {
+    this.coolDown = coolDown;
+    this.coolDowns = new Collection<string, number>();
+  }
+
+  add(key: string, coolDown = this.coolDown) {
+    this.coolDowns.set(key, Date.now() + coolDown);
+  }
+
+  check(key: string) {
+    const timestamp = this.coolDowns.get(key);
+
+    return timestamp ? timestamp - Date.now() : false;
+  }
+}
