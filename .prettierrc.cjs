fa8ba1022ac8ebe7620734415d6db88d50a24688
MRDGH2821 <mihirrabade@gmail.com>
2023-04-02 19:54:37 +0530

    chore: fix prettier config when using yarn

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,9 @@
+const { readdirSync } = require('fs');
+const { resolve } = require('path');
+const pluginsPath = resolve('./.yarn/unplugged/');
+const hasPlugins = readdirSync(pluginsPath).join().includes('prettier-plugin');
+
 module.exports = {
   ...require('prettier-config-mrdgh2821'),
+  pluginSearchDirs: hasPlugins ? pluginsPath : undefined,
 };
