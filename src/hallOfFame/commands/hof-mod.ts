f04a8ba2d1cdaf80bb84f67c6c408e50e0f762f4
Mihir <mihirrabade@gmail.com>
2023-04-03 05:56:04 +0000

    feat: segregate mod commands

--- src/hallOfFame/commands/hof-mod.ts
+++ src/hallOfFame/commands/hof-mod.ts
@@ -1,37 +1,20 @@
 import { Subcommand } from '@sapphire/plugin-subcommands';
-import { ApplicationCommandOptionType } from 'discord.js';
 import EnvConfig from '../../lib/EnvConfig';
 import type { JSONCmd } from '../../typeDefs/typeDefs';
-import { HALL_OF_FAME_ELEMENT_CHOICES } from '../lib/Constants';
 
 const cmdDef: JSONCmd = {
-  name: 'hall-of-fame',
-  description: 'Hall of Fame commands',
+  name: 'hof-mod',
+  description: 'Hall of Fame mod only commands',
   options: [
     {
       type: 1,
-      name: 'ranks',
-      description: 'Show Hall of Fame rankings',
-      options: [
-        {
-          name: 'element',
-          description: 'Select Element',
-          type: ApplicationCommandOptionType.String,
-          required: true,
-          choices: HALL_OF_FAME_ELEMENT_CHOICES,
-        },
-        {
-          name: 'crown_quantity',
-          description: 'Select Crown Quantity',
-          type: ApplicationCommandOptionType.String,
-          required: true,
-          choices: [
-            { name: 'One (1)', value: 'one' },
-            { name: 'Two (2)', value: 'two' },
-            { name: 'Three (3)', value: 'three' },
-          ],
-        },
-      ],
+      name: 'refresh',
+      description: 'Refreshes Hall of Fame cache',
+    },
+    {
+      type: 1,
+      name: 'publish',
+      description: 'Publishes the names of crown role holders',
     },
   ],
 };
@@ -41,12 +24,20 @@ export default class UserCommand extends Subcommand {
       ...options,
       name: cmdDef.name,
       description: cmdDef.description,
+      preconditions: ['ModOnly'],
       subcommands: [
         {
           name: cmdDef.options![0].name,
           type: 'method',
           chatInputRun(interaction) {
             return interaction.reply({ content: 'Hello this is cmd1!' });
+          },
+        },
+        {
+          name: cmdDef.options![1].name,
+          type: 'method',
+          chatInputRun(interaction) {
+            return interaction.reply({ content: 'Hello this is cmd2!' });
           },
         },
       ],
