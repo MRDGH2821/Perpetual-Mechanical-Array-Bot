58e02de338b0b6623681df9949f1da4da3b81508
Mihir <mihirrabade@gmail.com>
2023-04-04 06:58:28 +0000

    feat: update commands & add new sub command

--- src/hallOfFame/commands/hof-mod.ts
+++ src/hallOfFame/commands/hof-mod.ts
@@ -1,4 +1,5 @@
 import { Subcommand } from '@sapphire/plugin-subcommands';
+import { ApplicationCommandOptionType, ChannelType, MessageFlags } from 'discord.js';
 import EnvConfig from '../../lib/EnvConfig';
 import type { JSONCmd } from '../../typeDefs/typeDefs';
 
@@ -16,6 +17,21 @@ const cmdDef: JSONCmd = {
       name: 'publish',
       description: 'Publishes the names of crown role holders',
     },
+    {
+      type: ApplicationCommandOptionType.Subcommand,
+      name: 'setup',
+      description: 'Setup Hall of Fame channel',
+      options: [
+        {
+          type: ApplicationCommandOptionType.Channel,
+          name: 'forum_channel',
+          description: 'Select the forum channel where the updates will be posted',
+          required: true,
+          channel_types: [ChannelType.GuildForum],
+          channelTypes: [ChannelType.GuildForum],
+        },
+      ],
+    },
   ],
 };
 export default class UserCommand extends Subcommand {
@@ -30,14 +46,18 @@ export default class UserCommand extends Subcommand {
           name: cmdDef.options![0].name,
           type: 'method',
           chatInputRun(interaction) {
-            return interaction.reply({ content: 'Hello this is cmd1!' });
+            return interaction.reply({
+              content:
+                'Refresh initiated, please wait for a while before using hall of fame commands!',
+              flags: MessageFlags.Ephemeral,
+            });
           },
         },
         {
           name: cmdDef.options![1].name,
           type: 'method',
           chatInputRun(interaction) {
-            return interaction.reply({ content: 'Hello this is cmd2!' });
+            return interaction.reply({ content: 'Hall of Fame will be published soon.' });
           },
         },
       ],
