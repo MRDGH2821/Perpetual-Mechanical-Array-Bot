8c8202ed0e335901091ea4cd6c8273ce2469c287
Mihir <mihirrabade@gmail.com>
2023-04-11 09:46:32 +0000

    feat: add command error handler

---
+++ src/listeners/commands/chatInputCommands/chatInputCommandError.ts
@@ -0,0 +1,19 @@
+import type { ChatInputCommandErrorPayload, Events } from '@sapphire/framework';
+import { Listener } from '@sapphire/framework';
+import { codeBlock } from '@sapphire/utilities';
+import { MessageFlags } from 'discord.js';
+
+export default class UserEvent extends Listener<typeof Events.ChatInputCommandError> {
+  public async run(err: Error, payload: ChatInputCommandErrorPayload) {
+    const { interaction } = payload;
+    const errMsg = `${err}\n\n${codeBlock('md', JSON.stringify(err, null, 2))}`;
+    return interaction.deferred
+      ? interaction.editReply({
+        content: `An error occurred\n\n${errMsg}`,
+      })
+      : interaction.reply({
+        content: `An error occurred\n\n${errMsg}`,
+        flags: MessageFlags.Ephemeral,
+      });
+  }
+}
