9f5de9335a7d83382f111042ea5147ca24f67ec0
Mihir <mihirrabade@gmail.com>
2023-04-13 10:01:49 +0000

    fix: fix publisher function not able to publish

--- src/hallOfFame/listeners/HoFPublish.ts
+++ src/hallOfFame/listeners/HoFPublish.ts
@@ -16,97 +16,94 @@ import { crownProps } from '../lib/Utilities';
   name: 'Hall of Fame Publisher',
 })
 export default class HoFPublish extends Listener {
-  HallOfFameForum: ForumChannel | undefined;
-
   static dashLine = '-------------------------------------';
 
   public async run() {
-    const forumDB = await db
+    async function publish(element: ELEMENTS, HallOfFameForum: ForumChannel) {
+      const isUnaligned = () => element === 'unaligned';
+      const oneCrownEmbeds = await HallOfFameCache.generateEmbeds(element, 1);
+
+      const twoCrownEmbeds = isUnaligned()
+        ? undefined
+        : await HallOfFameCache.generateEmbeds(element, 2);
+
+      const threeCrownEmbeds = isUnaligned()
+        ? undefined
+        : await HallOfFameCache.generateEmbeds(element, 3);
+
+      const currentDate = new Date();
+      const props = crownProps(element);
+      const thread = await HallOfFameForum.threads.create({
+        name: `${props.plural} as of ${currentDate.toUTCString()} `,
+        message: {
+          content: `${props.description}`,
+        },
+      });
+
+      const insertDashLine = async () =>
+        thread.send({
+          content: HoFPublish.dashLine,
+        });
+
+      const insertEmbeds = async (embeds: APIEmbed[]) =>
+        thread.send({
+          embeds,
+        });
+
+      await insertDashLine();
+      const firstMsg = await insertEmbeds(oneCrownEmbeds);
+
+      if (twoCrownEmbeds) {
+        await insertDashLine();
+        await insertEmbeds(twoCrownEmbeds);
+      }
+
+      if (threeCrownEmbeds) {
+        await insertDashLine();
+        await insertEmbeds(threeCrownEmbeds);
+      }
+
+      container.logger.info(`Hall of fame for Element: ${element} sent!`);
+      return thread.send({
+        content: HoFPublish.dashLine,
+        components: [
+          new ActionRowBuilder<ButtonBuilder>({
+            components: [
+              new ButtonBuilder({
+                label: 'Back to first place',
+                style: ButtonStyle.Link,
+                emoji: '⬆',
+                url: firstMsg.url,
+              }),
+            ],
+          }),
+        ],
+      });
+    }
+
+    await db
       .collection('hall-of-fame-config')
       .doc('channel')
       .get()
-      .then((docSnap) => docSnap.data());
+      .then(async (docSnap) => {
+        const forumDB = docSnap.data();
 
-    if (!forumDB) {
-      throw new Error('Cannot fetch Forum channel for Hall Of Fame');
-    }
+        if (!forumDB) {
+          throw new Error('Cannot fetch Forum channel for Hall Of Fame');
+        }
 
-    const tvmGuild = await this.container.client.guilds.fetch(EnvConfig.guildId);
+        const tvmGuild = await this.container.client.guilds.fetch(EnvConfig.guildId);
+        return tvmGuild.channels.fetch(forumDB.forumId as string);
+      })
+      .then(async (forumChannel) => {
+        if (!(forumChannel instanceof ForumChannel)) {
+          throw new Error('Could not obtain text forum channel');
+        }
 
-    const forumChannel = await tvmGuild.channels.fetch(forumDB.forumId as string);
+        const publisher = (element: ELEMENTS) => publish(element, forumChannel);
 
-    if (!(forumChannel instanceof ForumChannel)) {
-      throw new Error('Could not obtain text forum channel');
-    }
-
-    this.HallOfFameForum = forumChannel;
-
-    await sequentialPromises(RELEASED_ELEMENTS, this.publish);
-  }
-
-  public async publish(element: ELEMENTS) {
-    if (!this.HallOfFameForum) {
-      throw new Error('Hall of Fame forum channel is undefined, please fetch');
-    }
-    const isUnaligned = () => element === 'unaligned';
-    const oneCrownEmbeds = await HallOfFameCache.generateEmbeds(element, 1);
-
-    const twoCrownEmbeds = isUnaligned()
-      ? undefined
-      : await HallOfFameCache.generateEmbeds(element, 2);
-
-    const threeCrownEmbeds = isUnaligned()
-      ? undefined
-      : await HallOfFameCache.generateEmbeds(element, 3);
-
-    const currentDate = new Date();
-    const props = crownProps(element);
-    const thread = await this.HallOfFameForum.threads.create({
-      name: `${props.plural} as of ${currentDate.toUTCString()} `,
-      message: {
-        content: `${props.description}`,
-      },
-    });
-
-    const insertDashLine = async () =>
-      thread.send({
-        content: HoFPublish.dashLine,
+        await sequentialPromises(RELEASED_ELEMENTS, publisher);
       });
-
-    const insertEmbeds = async (embeds: APIEmbed[]) =>
-      thread.send({
-        embeds,
-      });
-
-    await insertDashLine();
-    const firstMsg = await insertEmbeds(oneCrownEmbeds);
-
-    if (twoCrownEmbeds) {
-      await insertDashLine();
-      await insertEmbeds(twoCrownEmbeds);
-    }
-
-    if (threeCrownEmbeds) {
-      await insertDashLine();
-      await insertEmbeds(threeCrownEmbeds);
-    }
-
-    await thread.send({
-      content: HoFPublish.dashLine,
-      components: [
-        new ActionRowBuilder<ButtonBuilder>({
-          components: [
-            new ButtonBuilder({
-              label: 'Back to first place',
-              style: ButtonStyle.Link,
-              emoji: '⬆',
-              url: firstMsg.url,
-            }),
-          ],
-        }),
-      ],
-    });
-
-    container.logger.info(`Hall of fame for Element: ${element} sent!`);
+    // .catch(container.logger.error);
   }
 }
