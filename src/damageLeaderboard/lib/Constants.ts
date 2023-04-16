0461041edbc67dd2b814cb6c84e5225f6cc8a113
MRDGH2821 <mihirrabade@gmail.com>
2023-04-16 14:16:15 +0530

    chore: update option names

--- src/damageLeaderboard/lib/Constants.ts
+++ src/damageLeaderboard/lib/Constants.ts
@@ -2,23 +2,23 @@ import type { LBElements } from '../typeDefs/leaderboardTypeDefs';
 
 export const LEADERBOARD_DAMAGE_TYPE_CHOICES = <{ name: string; value: LBElements }[]>[
   {
-    name: "Traveler's Normal Attack 5th hit",
+    name: "Universal: Traveler's Normal Attack 5th hit",
     value: 'uni',
   },
   {
-    name: 'Palm Vortex - Max storm damage',
+    name: 'Anemo: Palm Vortex - Max storm damage',
     value: 'anemo',
   },
   {
-    name: 'Starfell Sword',
+    name: 'Geo: Starfell Sword',
     value: 'geo',
   },
   {
-    name: 'Lightening Blade',
+    name: 'Electro: Lightening Blade',
     value: 'electro',
   },
   {
-    name: 'Razor grass Blade',
+    name: 'Dendro: Razor grass Blade',
     value: 'dendro',
   },
 ];
