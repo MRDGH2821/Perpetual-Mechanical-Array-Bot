4f9a8bfd70c801e357e6a47db24398b5e5179c21
Mihir Rabade <mihirrabade@gmail.com>
2023-04-17 12:36:42 +0000

    feat: add new sub command register

--- src/damageLeaderboard/commands/leaderboard.ts
+++ src/damageLeaderboard/commands/leaderboard.ts
@@ -35,6 +35,53 @@ const cmdDef: JSONCmd = {
         },
       ],
     },
+    {
+      type: ApplicationCommandOptionType.Subcommand,
+      name: 'register',
+      description: 'Register damage score',
+      options: [
+        {
+          name: 'contestant',
+          description: 'Who made the score? (User ID can also be put)',
+          required: true,
+          type: ApplicationCommandOptionType.User,
+        },
+        {
+          name: 'element',
+          description: 'Which element was used?',
+          type: ApplicationCommandOptionType.String,
+          required: true,
+          choices: LEADERBOARD_DAMAGE_TYPE_CHOICES,
+        },
+        {
+          name: 'group_type',
+          description: 'Whether this score was made solo or not',
+          type: ApplicationCommandOptionType.String,
+          required: true,
+          choices: [
+            { name: 'Solo', value: 'solo' },
+            { name: 'Open', value: 'open' },
+          ],
+        },
+        {
+          name: 'score',
+          description: 'Score i.e. Damage value',
+          type: ApplicationCommandOptionType.Integer,
+          required: true,
+        },
+        {
+          name: 'proof_link',
+          description: 'Upload proof on traveler showcase channel & copy link to message',
+          type: ApplicationCommandOptionType.String,
+          required: true,
+        },
+        {
+          name: 'force_update',
+          description: 'Update the score forcefully even if lower (default false)',
+          type: ApplicationCommandOptionType.Boolean,
+        },
+      ],
+    },
   ],
 };
 export default class GuildCommand extends Subcommand {
@@ -63,6 +110,14 @@ export default class GuildCommand extends Subcommand {
             return pager(interaction);
           },
         },
+        {
+          name: cmdDef.options![1].name,
+          type: 'method',
+          chatInputRun: 'parseRegistration',
+        },
+      ],
+    });
+  }
       ],
     });
   }
