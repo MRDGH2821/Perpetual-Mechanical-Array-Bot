305f046be77f85684c981190ba27c57ec9bd05e6
Mihir Rabade <mihirrabade@gmail.com>
2023-04-18 12:11:02 +0000

    feat: add number to word converter

--- src/damageLeaderboard/lib/Utilities.ts
+++ src/damageLeaderboard/lib/Utilities.ts
@@ -108,3 +108,31 @@ export function parseDamageCategory(element: LBElements): ElementDamageCategorie
     }
   }
 }
+
+export function digitWord(digit: any) {
+  const parsed = digit instanceof Number ? digit : parseInt(digit, 10);
+  switch (parsed) {
+    case 0:
+      return 'zero';
+    case 1:
+      return 'one';
+    case 2:
+      return 'two';
+    case 3:
+      return 'three';
+    case 4:
+      return 'four';
+    case 5:
+      return 'five';
+    case 6:
+      return 'six';
+    case 7:
+      return 'seven';
+    case 8:
+      return 'eight';
+    case 9:
+      return 'nine';
+    default:
+      throw new Error(`Invalid digit provided: ${digit}`);
+  }
+}
