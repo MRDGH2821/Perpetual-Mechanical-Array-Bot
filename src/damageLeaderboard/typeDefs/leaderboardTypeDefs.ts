77332360f7ebe08d0202950e7687a21e0a3a8b84
Mihir Rabade <mihirrabade@gmail.com>
2023-04-18 09:57:41 +0000

    feat: add more types

--- src/damageLeaderboard/typeDefs/leaderboardTypeDefs.ts
+++ src/damageLeaderboard/typeDefs/leaderboardTypeDefs.ts
@@ -1,3 +1,5 @@
+import type { Message, User } from 'discord.js';
+
 export type ElementDamageCategories =
   | 'anemo-dmg-skill'
   | 'geo-dmg-skill'
@@ -12,3 +14,20 @@ export type N5Elements = 'uni';
 export type LBElements = SkillElements | N5Elements;
 
 export type GroupCategoryType = 'open' | 'solo';
+
+export type LBRegistrationArgs = {
+  contestant: User;
+  element: LBElements;
+  groupType: GroupCategoryType;
+  score: number;
+  proofMessage: Message;
+  shouldForceUpdate: boolean;
+};
+
+export type DBLeaderboardData = {
+  elementCategory: ElementDamageCategories;
+  proof: string;
+  score: number;
+  typeCategory: GroupCategoryType;
+  userID: User['id'];
+};
