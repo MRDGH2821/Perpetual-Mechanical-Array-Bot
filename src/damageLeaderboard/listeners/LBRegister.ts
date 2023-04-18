b6873cf1455dcce30755107d922c2a198c2c162e
Mihir Rabade <mihirrabade@gmail.com>
2023-04-18 12:11:22 +0000

    feat: add post registration function

--- src/damageLeaderboard/listeners/LBRegister.ts
+++ src/damageLeaderboard/listeners/LBRegister.ts
@@ -1,9 +1,11 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { container,Listener,ListenerOptions } from '@sapphire/framework';
+import { container, Listener, ListenerOptions } from '@sapphire/framework';
+import { sequentialPromises } from 'yaspr';
 import { PMAEventHandler } from '../../baseBot/lib/Utilities';
 import db from '../../lib/Firestore';
-import { parseDamageCategory,parseGroupType } from '../lib/Utilities';
-import type { DBLeaderboardData,LBRegistrationArgs } from '../typeDefs/leaderboardTypeDefs';
+import LeaderboardCache from '../lib/LeaderboardCache';
+import { digitWord, leaderboardProps, parseDamageCategory, parseGroupType } from '../lib/Utilities';
+import type { DBLeaderboardData, LBRegistrationArgs } from '../typeDefs/leaderboardTypeDefs';
 
 @ApplyOptions<ListenerOptions>({
   emitter: PMAEventHandler,
@@ -27,7 +29,34 @@ export default class LBRegister extends Listener {
       .set(dbData)
       .then(() => {
         container.logger.debug('Leaderboard Entry Submitted!');
+        this.postRegistration(args);
       })
       .catch(container.logger.error);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public async postRegistration(args: LBRegistrationArgs) {
+    await args.proofMessage.react('✅');
+
+    const rank = await LeaderboardCache.getRank(args.contestant.id, args.element, args.groupType);
+    const props = leaderboardProps(args.element);
+
+    await args.proofMessage.react(props.emoji);
+    if (rank < 100) {
+      const digits = rank.toString().split('');
+      const words = digits.map((digit) => digitWord(digit));
+      const wordsDone: string[] = [];
+
+      const reactWord = async (word: string) => {
+        if (!wordsDone.includes(word)) {
+          await args.proofMessage.react(word);
+          wordsDone.push(word);
+        } else {
+          await args.proofMessage.react('#️⃣');
+        }
+      };
+
+      await sequentialPromises(words, reactWord);
+    }
   }
 }
