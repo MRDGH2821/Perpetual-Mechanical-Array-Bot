dbee6d9b12eafc8d159de3a3c62c75166218700f
Mihir Rabade <mihirrabade@gmail.com>
2023-04-20 10:12:36 +0000

    feat: move function definition in run()
    Registration is now handled by LeaderboardCache class, hence the cleanup

--- src/damageLeaderboard/listeners/LBRegister.ts
+++ src/damageLeaderboard/listeners/LBRegister.ts
@@ -3,15 +3,9 @@ import { container, Listener, type ListenerOptions } from '@sapphire/framework';
 import type { EmojiIdentifierResolvable } from 'discord.js';
 import { sequentialPromises } from 'yaspr';
 import { PMAEventHandler } from '../../baseBot/lib/Utilities';
-import db from '../../lib/Firestore';
 import LeaderboardCache from '../lib/LeaderboardCache';
-import {
-  digitEmoji,
-  leaderboardProps,
-  parseDamageCategory,
-  parseGroupType,
-} from '../lib/Utilities';
-import type { DBLeaderboardData, LBRegistrationArgs } from '../typeDefs/leaderboardTypeDefs';
+import { digitEmoji, leaderboardProps } from '../lib/Utilities';
+import type { LBRegistrationArgs } from '../typeDefs/leaderboardTypeDefs';
 
 @ApplyOptions<ListenerOptions>({
   emitter: PMAEventHandler,
@@ -21,27 +15,6 @@ import type { DBLeaderboardData, LBRegistrationArgs } from '../typeDefs/leaderbo
 })
 export default class LBRegister extends Listener {
   public async run(args: LBRegistrationArgs) {
-    const dbData: DBLeaderboardData = {
-      elementCategory: parseDamageCategory(args.element),
-      proof: args.proofMessage.url,
-      score: args.score,
-      typeCategory: parseGroupType(args.groupType),
-      userID: args.contestant.id,
-    };
-
-    await db
-      .collection(`${dbData.elementCategory}-${dbData.typeCategory}`)
-      .doc(dbData.userID)
-      .set(dbData)
-      .then(() => {
-        container.logger.debug('Leaderboard Entry Submitted!');
-        this.postRegistration(args);
-      })
-      .catch(container.logger.error);
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  public async postRegistration(args: LBRegistrationArgs) {
     try {
       await args.proofMessage.react('✅');
 
