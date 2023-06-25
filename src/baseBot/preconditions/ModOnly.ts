f4ed6c5ea1d73f186cee2d72d0a35f733f71a13d
MRDGH2821 <mihirrabade@gmail.com>
2023-06-25 21:05:59 +0530

    refactor: remove duplicate code

--- src/baseBot/preconditions/ModOnly.ts
+++ src/baseBot/preconditions/ModOnly.ts
@@ -1,30 +1,19 @@
 import { Precondition } from '@sapphire/framework';
-import type {
-  APIInteractionGuildMember,
-  ChatInputCommandInteraction,
-  ContextMenuCommandInteraction,
-  GuildMember,
-} from 'discord.js';
+import type { ChatInputCommandInteraction, ContextMenuCommandInteraction } from 'discord.js';
 import { isStaff } from '../lib/Utilities';
 
 export default class ModOnly extends Precondition {
   public override chatInputRun(interaction: ChatInputCommandInteraction) {
-    if (interaction.member) {
-      if (this.checkStaff(interaction.member)) {
-        return this.ok();
-      }
-      return this.error({
-        message: 'Only Mods can use this',
-      });
-    }
-    return this.error({
-      message: 'This command can only be used in server',
-    });
+    return this.logicRun(interaction);
   }
 
   public override contextMenuRun(interaction: ContextMenuCommandInteraction) {
+    return this.logicRun(interaction);
+  }
+
+  private logicRun(interaction: ChatInputCommandInteraction | ContextMenuCommandInteraction) {
     if (interaction.member) {
-      if (this.checkStaff(interaction.member)) {
+      if (isStaff(interaction.member)) {
         return this.ok();
       }
       return this.error({
@@ -34,11 +23,6 @@ export default class ModOnly extends Precondition {
     return this.error({
       message: 'This command can only be used in server',
     });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  private checkStaff(member: GuildMember | APIInteractionGuildMember) {
-    return isStaff(member);
   }
 }
 
