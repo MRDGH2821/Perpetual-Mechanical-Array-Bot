fc9d7b39eeae5fd08913390b9f7d9476c7a1413f
MRDGH2821 <mihirrabade@gmail.com>
2023-06-25 15:31:50 +0530

    chore: remove Dockerfile steps and add it to setup.sh script
    Remove comments and unused properties from devcontainer.json file

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,7 @@
+echo "Installing Firefox..."
+sudo apt-get update
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get install -y firefox-esr
+
+echo "Installing dependencies using yarn"
+yarn install
