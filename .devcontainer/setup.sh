0b7a12642e728f61e56c13912950b549d149b414
MRDGH2821 <ask.mrdgh2821@outlook.com>
2023-10-20 13:57:11 +0000

    chore: remove setup script

--- .devcontainer/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing Firefox..."
-sudo apt-get update
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -y firefox-esr
-
-echo "Installing dependencies using yarn"
-yarn install
